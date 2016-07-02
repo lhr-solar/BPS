7e21aa10643f20b1edc47287b8f40de781f15c26
Corey Hulse <cohulse@asu.edu>
2016-07-02 09:29:46 -0700

    Python Cloud
    Allows for file and data exchange, and even ssh functionality to a
    remote server through python

--- Single Channel Method/ClientHandler.py
+++ Single Channel Method/ClientHandler.py
@@ -3,54 +3,64 @@ import SocketConnection #what directly talks to the client
 
 class ConnectionHandler(threading.Thread)
 
-    def __init__(self, host, port, con, server):
+    def __init__(self, host, port, con, server, testing= False):
         self.host = host
         self.port = port
-        
+
+        #starts up a connection hadnling object, handles all data transfer        
         self.connection = SocketConnection(addr, port, con)
 
         self.server = server
 
+        #recieve the client password and check if it is a known password
         password = self.connection.recvieveData()
-        privilege = server.checkPassword(password)
+        if not testing:
+            self.privilege = server.checkPassword(password)
 
-        self.quit = False
+            self.quit = False
 
-        if privilege == False:
-            self.quit = True
+            if self.privilege == False:
+                self.quit = True
 
+        else:
+            self.privilege = 'ADMIN'
+    
+        #runs the constructor for the thread class as well as this constructor
         super(ConnectionHandler, self).__init__()
         
     def run(self):
+        #sends the initial command prompt
         self.transmitSocket.sendData(os.getcwd() + '>>')
-        
+
+        #the thread will complete on its own once a source triggers its exit flag
         while not self.quit:
+            #read in user commands and execute their corresponding functions
             self.inData = self.connection.recieveData()
 
             if self.inData = None:
                 pass
             
-            elif self.inData[:3] == "-uf":
+            elif self.inData[:3] == "-uf": #the client is uploading a file
                 try:
                     self.connection.recieveFile(self.inData[4:])
    
                 except:
                     self.connection.sendData('Error Encountered: ' + str(sys.exc_info()[0]) + '\n')
                 
-            elif self.inData[:3] == "-df":
+            elif self.inData[:3] == "-df": #the client wants to download a file
                 try:
                     self.connection.recieveFile(self.inData[4:])
                 except:
                     self.connection.sendData('Error Encountered: ' + str(sys.exc_info()[0]) + '\n')
                 
-            elif self.inData[:4] == "-ufs":
+            elif self.inData[:4] == "-ufs": #the client wants to upload multiple files whos names are seperated by spaces
                 fileNames = self.inData[5:]
                 fileNames = fileNames.split()
                     
                 self.connection.recieveFiles()
 
                 
-            elif self.inData[:4] == "-dfs"
+            elif self.inData[:4] == "-dfs" #the client wants to download multiple files whos names are seperated by spaces
                 try:
                     files = self.inData[5:]
                     files = files.split()
@@ -58,35 +68,35 @@ class ConnectionHandler(threading.Thread)
                 except:
                     self.connection.sendData('Error Encountered: ' + str(sys.exc_info()[0]) + '\n')
 
-            elif self.inData[:3] == "-ud":
+            elif self.inData[:3] == "-ud": #the client wants to upload a directory 
                 try:
                     self.connection.recieveDir(self.inData[4:])
 
                 except:
                     self.connection.sendData('Error Encountered: ' + str(sys.exc_info()[0]) + '\n')
 
-            elif self.inData[:3] == "-dd":
+            elif self.inData[:3] == "-dd": #the client wants to download a directory
                 try:
                     self.sendDir(self.inData[4:])
                 except:
                     self.connection.sendData('Error Encountered: ' + str(sys.exc_info()[0]) + '\n')
 
-            elif self.inData[:3] == "-ce":
+            elif self.inData[:3] == "-ce": #connect to an email account remotely
                 data = self.inData[4:]
                 data = data.split()
                 self.smtp = SmtpConnection(data[0], data[1], data[2])
                 
-            elif self.inData[:3] == "-sm":
+            elif self.inData[:3] == "-sm": #send an email of the specified files
                 data = self.inData[4:]
                 data = data.split(',')
                 files = data[2].split()
                 self.smtp.sendMail(data[0], data[1], files)
 
-            elif self.inData[:3] == '-wq':
+            elif self.inData[:3] == '-wq': #close the connection between client and server
                 self.quit = True
                 self.closeChannel()
                 
-            elif self.inData[:2] == '-h':
+            elif self.inData[:2] == '-h': #displays all the possible commands
                 data = "-uf\t\tUpload a file, command followed by the file name\n"
                 data += "-df\t\tDownload a file, command followed by the file name\n"
                 data += "-ufs\t\tUpload Files, command followed by list of file names seperated by spaces\n"
@@ -103,20 +113,21 @@ class ConnectionHandler(threading.Thread)
                 except:
                     self.connection.sendData('Error Encountered: ' + str(sys.exc_info()[0]) + '\n')
                 
-            elif self.inData[:3] == "-cd" and self.privilege == "ADMIN":
+            elif self.inData[:3] == "-cd" and self.privilege == "ADMIN": #allows the suer to change server directories if they are an admin
                 try:
                     os.chdir(self.inData[4:])
                 except:
                     self.connection.sendData('Error Encountered: ' + str(sys.exc_info()[0]) + '\n')
                 
-            elif self.privilege == "ADMIN":
+            elif self.privilege == "ADMIN": #unknown commands are sent to the command line of the server if the client is an admin
                 cmd = subprocess.Popen(self.inData, shell= True, stdout= subprocess.PIPE, stderr= subprocess.PIPE)
                 cmd = cmd.stdout.read() + cmd.stderr.read()
                 self.connection.sendData(cmd)
 
             else:
                 self.connection.sendData("Access Not Allowed to those commands\n")
-                
+
+            #send back the prompt to let the client know the server is ready for the next command
             self.connection.sendData(os.getcwd() + ">> ")
 
             
@@ -144,5 +155,12 @@ class ConnectionHandler(threading.Thread)
     
 
 if __name__ == "__main__":
-    
+    #for testing purposes
+    server = socket.socket()
+    server.bind(('127.0.0.1', 5000))
+    server.listen(5)
 
+    con, addr = server.accept()
+    handler = ClientHandler('127.0.0.1', 5000, con, None, True)
+    handler.start()
+    
--- Single Channel Method/serverClient.py
+++ Single Channel Method/serverClient.py
@@ -11,6 +11,9 @@ def main(host, port, password):
     #sends over the password
     socket.sendData(password)
 
+    #print the inital server directory
+    data = socket.recieveData()
+    print(data, end= '')
     command = ""
 
     while command != 'wq':
@@ -19,31 +22,49 @@ def main(host, port, password):
 
         if command = None:
             pass
-            print('>> ')
+            print(data,end= '')
 
         #the users commands trigger different functions within the socketConnection which deals with the servers socket
         else:
             if command[:3] == "-uf": #upload file
-                socket.sendFile(command[4:])
+                try:
+                    socket.sendFile(command[4:])
+                except:
+                    pass
                 
             elif command[:3] == "-df": #download file
-                socket.recieveFile(command[4:])
+                try:
+                    socket.recieveFile(command[4:])
+                except:
+                    pass
                 
             elif command[:4] == "-ufs": #upload files, entered as names sperated by spaces
-                files = command[5:]
-                files = files.split()
-                socket.sendFiles(files)
+                try:
+                    files = command[5:]
+                    files = files.split()
+                    socket.sendFiles(files)
+                except:
+                    pass
                 
             elif command[:4] == "-dfs": #download files, entered as names seperated by spaces
-                files = command[5:]
-                files = files.split()
-                socket.recieveFiles(files)
-                 
+                try:
+                    files = command[5:]
+                    files = files.split()
+                    socket.recieveFiles(files)
+                 except:
+                     pass
+                    
             elif command[:3] == "-ud": #upload directory
-                socket.sendDir(command[4:])
+                try:
+                    socket.sendDir(command[4:])
+                except:
+                    pass
                 
             elif command[:3] == "-dd": #download directory
-                socket.recieveDir(command[4:])
+                try:
+                    socket.recieveDir(command[4:])
+                except:
+                    pass
                 
             elif command[:3] == "-wq": #ends the connection and program
                 socket.quit = True
