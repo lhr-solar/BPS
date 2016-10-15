ca98e259d64f2750efcd8a15753bdd5198d0b3cb
Corey Hulse <cohulse@asu.edu>
2016-10-15 17:37:28 -0500

    removed outdated code
    read title

--- testReading.py
+++
@@ -1,8 +0,0 @@
-import sys
-with open('testFile.txt', 'w') as file:
-    file.write('testing testing testing testing testing testing')
-with open('testFile.txt', 'r') as file:
-    data = "asdf".encode('utf-8')
-    while data:
-        data = file.read(5)
-        print(data)
