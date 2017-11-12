ea3584b98a6302cd0d75720536335fb1530fa3f3
Abel Philips <31525042+abelph@users.noreply.github.com>
2017-11-12 16:01:20 -0600

    Create PID.h

---
+++ Signal Processing/PID.h
@@ -0,0 +1,30 @@
+#ifndef MBED_CAN_LIB_H
+#define MBED_CAN_LIB_H
+
+#include "mbed.h"
+
+class PID {
+    
+    
+    
+    public:
+        PID(int[] constants);
+        float generatePIDOutput(float error);
+        void setDesiredState(float newDesState);
+        
+    private:
+        float generateP(float error);
+        float generateI(float error);
+        float generateD(float error);
+        float generateError(float currentState);
+        float desiredState;
+        Timer t;
+        bool reset = false;
+        float integral;
+        float lastError;
+        float timeOfLastMeasurement;
+        
+    
+}
+
+#endif
