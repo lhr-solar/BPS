97333b74d2239d241081388153d9c288e166f15b
SijWoo <sijin.woo@utexas.edu>
2018-02-10 12:50:25 -0600

    CAN_lib test code updated

--- Main Board/CAN_libTest.cpp
+++ Main Board/CAN_libTest.cpp
@@ -2,74 +2,86 @@
 #include "CAN.h"
 #include "CAN_lib.h"
 
-DigitalOut heartBeat(LED1);
+DigitalOut led4(LED4);
 
-CAN_Interrupt_Handler can1(PB_8, PB_9, 100000);     // Pins for Nucleo-F072RB
+//Serial pcConnection(SERIAL_TX, SERIAL_RX);
+
+CAN_Interrupt_Handler can1(PD_0, PD_1, 3, 100000);     // Address 0x21, priority 3
 //CAN_Interrupt_Handler can2(PA_11, PA_12, 100000);
 //CAN canBus(PD_0, PD_1);   // for F429ZI
-//CANMessage canMailBox;
+CANMessage canMailBox;
+CANMessage receiverMailBox;
 
-DigitalOut messageSignalLED(LED1);
-InterruptIn sendButton(BUTTON1);
-CAN testCANbus(PA_11, PA_12);       // Pins for Nucleo-F072RB
+//DigitalOut messageSignalLED(LED1);
+//InterruptIn sendButton(BUTTON1);
+//CAN testCANbus(PA_11, PA_12);       // Pins for Nucleo-F072RB
 
+// transmitter prototypes
 int CANaddress = 1337;
 void sendTestMessage(); 
+void setup();
+void canInterupptRoutine();
 
-void setup()
-{
-    sendButton.rise(&sendTestMessage);
-    testCANbus.frequency(100000);
+void ledBlink(){
+    led4 != led4;
 }
-
-int main() 
-{
-    setup();
-    while(1)
-    {
-       ;
-    }
-    
-}
-
+/*
+// for transmitter
 void sendTestMessage()
 {
-    messageSignalLED = 1;
+    //messageSignalLED = 1;
     wait(3);
     char data[] = "Hello";
     CANMessage testMessage(CANaddress, data, sizeof(data));
     testCANbus.write(testMessage);
-    messageSignalLED = 0;   
-}
-
-
-/*
-void canInterupptRoutine();
+    //messageSignalLED = 0;   
+}*/
 
 void setup()
 {
-    canBus.frequency(100000);
-    canBus.attach(&canInterupptRoutine, CAN::RxIrq);
+    //canBus.frequency(100000);
+    //canBus.attach(&ledBlink, CAN::RxIrq);
+    //pcConnection.baud(9600);
+    //pcConnection.format(8, Serial::None, 1);
+    //sendButton.rise(&sendTestMessage);
+    //testCANbus.frequency(100000);
+    //canBus.frequency(100000);
+    //anBus.attach(&canInterupptRoutine, CAN::RxIrq);
 }
 
 int main() 
 {
-    setup();    
+    //setup();    
     while(1) 
     {
         ;
     }
 }
 
+/*
 void canInterupptRoutine()
 {
     canBus.read(canMailBox);
     if(canMailBox.data[0] == '1')
     {
-        heartBeat = 1;
+        led3 = 1;
     }
     else
     {
-        heartBeat = 0;
+        led3 = 0;
     }
-}*/
+}
+
+void receiveMessage()
+{
+ canBus.read(receiverMailBox);
+ if(receiverMailBox.data[0] == '1')
+ {
+    myLed = 1;
+ }
+ else
+ {
+    myLed = 0;
+ }
+}
+*/
