6031b9cb9e1bb0c2327b7c308df16aaa883b283e
Sijin Woo <sijin.woo@utexas.edu>
2018-09-04 18:18:35 -0500

    Better documented Contactor code.

--- BPS/BeVolt/Contactor.cpp
+++ BPS/BeVolt/Contactor.cpp
@@ -1,42 +1,53 @@
+// Contactor.cpp
+/**
+ * Driver for contactor (EV200) relay that disconnects the battery from car. Uses TI DRV102 driver
+ * to deliver necessary current to energize the contactor
+ * @authors Sijin Woo, Corey Hulse
+ * @lastRevised 9/4/2018
+ */
+
 #include "Contactor.h"
 
-// class constructor
+/** Constructor
+ * Creates contactor instance for a single contactor relay. The
+ * input pin is used to signify a fault in the driver. The output
+ * pin is used to turn on the contactor through the TI DRV102 driver.
+ * @param Pin output
+ * @param Pin input
+ */
 Contactor::Contactor(PinName out, PinName in) : signal(out), flag(in){
     signal = 0;
 }
 
-//******************************************************************************
-// openContactor
-// opens contactor by setting signal pin to 0
-// Input: None
-// Output: None
+/** openContactor
+ * Sends signal to TI DRV102 to open the contactor. This disconnects the 
+ * battery pack from the car.
+ */
 void Contactor::openContactor(){
     signal = 0;
 }
 
-//******************************************************************************
-// closeContactor
-// closes contactor by setting signal pin 1
-// Input: None
-// Output: None
+/** closeContactor
+ * Sends signal to TI DRV102 to close the contactor. This connects the battery
+ * pack to the car.
+ */
 void Contactor::closeContactor(){
     signal = 1;
 }
 
-//******************************************************************************
-// getFlagState
-// checks the flag if the contactor is operating correctly
-// Input: None
-// Output: returns 1 if functioning correctly, 0 if not
+/** getFlagState
+ * Gets the state of the flag. The flag is normally high but if a fault occurs,
+ * the flag will be set to low
+ * @return 1 is working properly, 0 if fault
+ */
 int Contactor::getFlagState(){
     return flag;
 }
 
-//******************************************************************************
-// getSignalState
-// checks if the contactor is on or off
-// Input: None
-// Output: returns 1 if closed, 0 if open
+/** getSignalState
+ * Gets the state of the output signal.
+ * @return 1 if signal is high (contactor is closed), 0 if signal is low (contactor is open)
+ */
 int Contactor::getSignalState(){
     return signal;
 }
