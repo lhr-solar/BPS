fe05260b870be4e92aa7cadc95f0ebe73a93f941
Chase Block <git@chaseblock.com>
2019-02-08 14:29:02 -0600

    Some more terminal work.

--- BeVolt/User/Terminal.h
+++ BeVolt/User/Terminal.h
@@ -14,38 +14,33 @@
 #include "Voltage.h"
 #include "Temperature.h"
 #include "Gyro.h"
+#include "Contactor.h"
+
+void Terminal_Halt();
 
 void Terminal_CheckInput(void);
 
 void Terminal_HandleInput(char * op, char * arg);
 
-char ** Terminal_splitOps (char * input);
+void Terminal_CurrentStatus(void);
 
-void Terminal_currentStatus(void);
+void Terminal_VoltageStatus(void);
 
-void Terminal_voltageStatus(void);
+void Terminal_TemperatureStatus(uint8_t isCharging);
 
-void Terminal_temperatureStatus(void);
+void Terminal_ContactorStatus(void);
 
-void Terminal_contactorStatus(void);
-
-void Terminal_setContactor(int status);
+void Terminal_SetContactor(int status);
 
 /**
  * @param the axes to read from, where 0 = all, 1 = x, 2 = y, 3 = z
  */
-void Terminal_gyroStatus(uint8_t axes);
+void Terminal_GyroStatus(uint8_t axes);
 
-void Terminal_watchdogStatus(void);
+void Terminal_WatchdogStatus(void);
 
-void Terminal_eepromStatus(void);
+void Terminal_EepromStatus(void);
 
-void Terminal_canStatus(void);
-
-void Terminal_spiStatus(void);
-
-void Terminal_i2cStatus(void);
-
-void Terminal_helpMenu(void);
+void Terminal_HelpMenu(void);
 
 #endif
