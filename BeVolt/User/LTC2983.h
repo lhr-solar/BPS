d45ed3dd05a0e9abde23c9c66af1105d9350d4e4
Sihyung <sihyungwoo@gmail.com>
2019-05-04 11:41:45 -0500

    Updated Header File for LTC2983

--- BeVolt/User/LTC2983.h
+++ BeVolt/User/LTC2983.h
@@ -17,41 +17,7 @@
  * Initializes SPI pins
  * Initializes and configures LTC2983 chip 
  */
-void LTC2983_Init(void){
-	GPIO_InitTypeDef GPIO_InitStruct;
-	SPI_InitTypeDef SPI_InitStruct;
-	
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);	// 1) Initialize GPIO port A clock
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_SPI3, ENABLE);
-	
-	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5;		// 2) Initialize which pins to use
-	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;							// 3) Set PA8 and PA9 as alternate function
-	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;							// 4) Set the resistor to pull-up
-	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;				// 5) Initialize the speed of communication as 25 MHz
-	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;						// 6) Set to open drain
-	GPIO_Init(GPIOB, &GPIO_InitStruct);
-	
-	GPIO_PinAFConfig(GPIOB, GPIO_PinSource3, GPIO_AF_SPI3);
-	GPIO_PinAFConfig(GPIOB, GPIO_PinSource4, GPIO_AF_SPI3);
-	GPIO_PinAFConfig(GPIOB, GPIO_PinSource5, GPIO_AF_SPI3);
-	
-	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_6;
-	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
-	GPIO_Init(GPIOB, &GPIO_InitStruct);
-	
-	SPI_InitStruct.SPI_FirstBit = SPI_FirstBit_MSB;
-	SPI_InitStruct.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_2;
-	SPI_InitStruct.SPI_NSS = SPI_NSS_Soft;
-	SPI_InitStruct.SPI_CPHA = SPI_CPHA_1Edge;
-	SPI_InitStruct.SPI_CPOL = SPI_CPOL_Low;
-	SPI_InitStruct.SPI_CRCPolynomial = 0x1;
-	SPI_InitStruct.SPI_DataSize = SPI_DataSize_8b;
-	SPI_InitStruct.SPI_Direction = SPI_Direction_Rx | SPI_Direction_Tx;
-	SPI_InitStruct.SPI_Mode = SPI_Mode_Master;
-	
-	SPI_Init(SPI3, &SPI_InitStruct);
-	SPI_Cmd(SPI3, ENABLE);
-}
+void LTC2983_Init(void);
 
 /** LTC2983_Ready
  * Checks if LTC2984 is ready after startup
@@ -71,11 +37,5 @@ int32_t LTC2983_MeasureSingleChannel(void /*uint8_t *channelAddr*/);
  * Sends command to LTC2983 to gather and save all ADC values
  * @return unsigned int 16-bit measurements from all ADCs
  */
-uint16_t *LTC2983_Measure(void){
-	
-}
 
-
-uint32_t LTC2983_ReadChannel(uint16_t channel);
-
-#endif
+//#endif
