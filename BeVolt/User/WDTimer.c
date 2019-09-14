f522c6cfe9de6e2eef3fb4499f4fbd657b4442ca
Manolo Alvarez Cristiano <35587627+malvarez0722@users.noreply.github.com>
2019-09-14 15:20:32 -0500

    Disabled Window Watchdog for testing purposes. Independent Watchdog still active with at time out of 4.095 seconds

--- BeVolt/User/WDTimer.c
+++ BeVolt/User/WDTimer.c
@@ -20,11 +20,13 @@
  */
 void WDTimer_Init(void){
 	/* Window Watchdog Init */
+	/*
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_WWDG, ENABLE);    // Initializes WWDG clock at APB1 Bus speed
 	WWDG_EnableIT();                                        // Enables the WWDG Early Wakeup interrupt(EWI)
 	WWDG_SetCounter(WWDG_CFR_W);                            // Sets the WWDG counter value. 
 	WWDG_SetPrescaler(WWDG_Prescaler_8);                    // Sets the WWDG Prescaler.
 	WWDG_SetWindowValue(WWDG_CFR_W);                        // Sets the WWDG window value.
+	*/
 	
 	/* Independent Watchdog Init */
 	FLASH_OB_Unlock();                                      // Enable the FLASH option control register access
@@ -33,7 +35,7 @@ void WDTimer_Init(void){
 	FLASH_OB_Lock();                                        // Disable the FLASH option control register
 	RCC_LSICmd(ENABLE);                                     // Initializes LSI clock at 32KHz used for IWDG
 	IWDG_WriteAccessCmd(IWDG_WriteAccess_Enable);           // Enable access to the IWDG_PR and IWDG_RLR registers
-	IWDG_SetPrescaler(IWDG_Prescaler_32);                  // Prescaler divider feeding the counter clock
+	IWDG_SetPrescaler(IWDG_Prescaler_32);                   // Prescaler divider feeding the counter clock
 	IWDG_SetReload(IWDG_RLR_RL);                            // Reload value set to 0xFFF
 }
 
@@ -41,7 +43,7 @@ void WDTimer_Init(void){
  * Starts the Watch Dog Timer count down
  */
 void WDTimer_Start(void){
-	WWDG_Enable(WWDG_CFR_W);                     // Activates Window Watchdog
+	//WWDG_Enable(WWDG_CFR_W);                     // Activates Window Watchdog
 	IWDG_Enable();                               // Starts the watchdog
 }
 
@@ -49,14 +51,16 @@ void WDTimer_Start(void){
  * Reloads the Watch Dog Timer. Does not stop the counter.
  */
 void WDTimer_Reset(void){
-	WWDG_Enable(WWDG_CFR_W);                    // Reloads the Window Watchdog Timer
+	//WWDG_Enable(WWDG_CFR_W);                    // Reloads the Window Watchdog Timer
 	IWDG_ReloadCounter();                       // Reloads the Independent Watchdog Timer
 }
 
 /** WWDG_IRQHandler
  * Interrupts every 41.94304 ms when running at APB1 Bus Speed of 50MHz
  */
+/*
 void WWDG_IRQHandler(void){
 	Contactor_Off();                             // Fault: open contactor
 	WWDG_ClearFlag();                            // Clear EWI Flag
 }
+*/
