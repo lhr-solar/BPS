36371c4214f27f37fecaaee05cb3323df8aad326
russellsutton1 <36014752+russellsutton1@users.noreply.github.com>
2019-09-21 14:23:36 -0500

    i messed up but still just making branch visible

--- BeVolt/Periphs/src/ADC.c
+++ BeVolt/Periphs/src/ADC.c
@@ -1,54 +1,3 @@
-<<<<<<< HEAD
-/** ADC.c
- * Program for UTSVT BeVolt's Battery Protection System ADC initialization
- */
-
-#include <stdint.h>
-#include "stm32f4xx.h"
-#include "ADC.h"
-
-/** ADC_InitHilo
- * Initializes PA2 (high prec) and PA3 (low prec) as ADC input pins.
- */
-void ADC_InitHilo(void){
-	GPIO_InitTypeDef GPIO_InitStruct;													// make structure
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE); 		// enable clock portA
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
-	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3;				// setting pin2? and pin 3? high
-	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AN;									// Settting mode to ???? where did i find the stuff like AN
-	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_25MHz;						// setting clock?
-	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;								// ^^ ^^
-	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_DOWN;
-	GPIO_Init(GPIOA,&GPIO_InitStruct);												// calling GPIO_Init function with PortA and InitStruct
-
-	ADC_InitTypeDef ADC_InitStruct;														// Init ADC
-	ADC_InitStruct.ADC_ContinuousConvMode = DISABLE;
-	ADC_InitStruct.ADC_DataAlign = ADC_DataAlign_Right;
-	ADC_InitStruct.ADC_ExternalTrigConv = DISABLE;
-	ADC_InitStruct.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;
-	ADC_InitStruct.ADC_NbrOfConversion = 2;										// one sample.. fast but not precise
-	ADC_InitStruct.ADC_Resolution = ADC_Resolution_12b;				// 12 bit precision
-	ADC_InitStruct.ADC_ScanConvMode = DISABLE;								// single or all channels
-	ADC_Init(ADC1, &ADC_InitStruct);													// initialize
-	ADC_Cmd(ADC1, ENABLE);																		// command
-}
-
-/** ADC_ReadHigh
- * Returns the adc value of PA2 (high prec)
- * @return adc value in high prec adc
- */
-uint32_t ADC_ReadHigh(void){
-
-}
-
-/** ADC_ReadLow
- * Returns the adc value of PA3 (low prec)
- * @return adc value of low prec adc
- */
-uint32_t ADC_ReadLow(void){
-
-}
-=======
 /** main.c
  * Program for UTSVT BeVolt's Battery Protection System ADC initialization
  * @authors Garrett Wong, Sijin Woo
@@ -61,17 +10,17 @@ uint32_t ADC_ReadLow(void){
 /** ADC_InitHilo
  * Initializes PA2 (high prec) and PA3 (low prec) as ADC input pins.
  */
-void ADC_InitHilo(void){	
+void ADC_InitHilo(void){
 	GPIO_InitTypeDef GPIO_InitStruct;													// make structure
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE); 		// enable clock portA
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
 	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3;				// setting pin2? and pin 3? high
 	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AIN;									// Settting mode to ???? where did i find the stuff like AN
 	//GPIO_InitStruct.GPIO_Speed = GPIO_Speed_25MHz;						// setting clock?
-	//GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;								// ^^ ^^ 
+	//GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;								// ^^ ^^
 	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
 	GPIO_Init(GPIOA,&GPIO_InitStruct);												// calling GPIO_Init function with PortA and InitStruct
-	
+
 	ADC_InitTypeDef ADC_InitStruct;														// Init ADC
 	ADC_InitStruct.ADC_ContinuousConvMode = DISABLE;
 	ADC_InitStruct.ADC_DataAlign = ADC_DataAlign_Right;
@@ -96,7 +45,7 @@ uint32_t ADC_ReadHigh(void){
 	while (!ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC));
 	return ADC1->DR & 0x00000FFF;
 }
-	
+
 /** ADC_ReadLow
  * Returns the adc value of PA3 (low prec)
  * @return adc value of low prec adc
@@ -110,19 +59,19 @@ uint32_t ADC_ReadLow(void){
 }
 	// INITIALIZE ADC PIN "ALTERNATE FUNCTION"
 	// SAME THING FOR OTHER ONES
-	// RETURN 
+	// RETURN
 
 /** ADC_Conversion
  * Returns the converted current value given the ADC readings
  * @returns converted current
  */
 uint32_t ADC_Conversion (uint32_t ADC_Reading){
- // note Fred's board already converts the -4 to 4 volts to 0-4 
+ // note Fred's board already converts the -4 to 4 volts to 0-4
  //uint32_t convertedVoltage = ((ADC_Reading*4.096)/2047)*1000; // converts ADC reading to a voltage
  uint32_t convertedVoltage = (((ADC_Reading*3300)/4096));
  int32_t convertedCurrent = (convertedVoltage-2048)*(100/4); // takes voltage and converts to a current reading, given linear slope
  return convertedCurrent;
- //return convertedVoltage;	
+ //return convertedVoltage;
 }
 
 /** ADC_ChooseHiLo
@@ -137,5 +86,3 @@ uint32_t ADC_ChooseHiLo (uint32_t highReading, uint32_t lowReading){
 	}
 	return ADC_Conversion(highReading);*/
 }
-
->>>>>>> fc94aab... commiting so the branch becomes accessible
