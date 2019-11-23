f35841cdf45904ffcae3870038e1b3facaaf9c77
ClarkPoon <clarkpoon@utexas.edu>
2019-11-23 17:03:23 -0600

    added functions for getting commands from uart fifo

--- BeVolt/Periphs/src/fifo.c
+++ BeVolt/Periphs/src/fifo.c
@@ -4,11 +4,13 @@
 
 
 #define size 32
+#define carriage_return 0x0d
 
 typedef struct Fifo {
 	char queue[size];
 	uint8_t head;
 	uint8_t tail;
+	bool hasCommand;//use when fifo is storing commands received over uart
 	}Fifo;
 
 
@@ -16,6 +18,7 @@ typedef struct Fifo {
 void fifoInit(Fifo *fifo){
 	fifo->head = 0;
 	fifo->tail = 0;
+	fifo->hasCommand = false;
 }
 
 bool fifoIsEmpty(Fifo fifo){
@@ -31,6 +34,11 @@ void fifoPut(Fifo *fifo, char newChar){
 	if(fifoIsFull(*fifo) == false){
 		fifo->queue[fifo->head] = newChar;
 		fifo->head = ((fifo->head + 1) % size);
+		
+		//for if fifo is used to store commands given by uart
+		if (newChar == carriage_return){
+			fifo->hasCommand = true;
+		}
 	}
 }
 
@@ -45,4 +53,21 @@ char fifoGet(Fifo *fifo){
 	return DANGER;
 }
 
+
+//for using fifo to store commands received from UART
+//fails if multiple commands are stored in queue
+//preconditions: fifo has a command stored in it, buffer is large enough to hold command
+//parameters: fifo with command stored in it, buffer to receive stored command
+//fills buffer with received command
+void getCommand(Fifo *fifo, char buffer[]){
+	if (fifo->hasCommand){
+		int i = 0;
+		while(fifo->queue[fifo->tail] != carriage_return){
+			buffer[i] = fifoGet(fifo);
+			i++;
+		}
+		fifoGet(fifo);//get rid of carriage return
+		fifo->hasCommand = false;
+	}
+}
 
