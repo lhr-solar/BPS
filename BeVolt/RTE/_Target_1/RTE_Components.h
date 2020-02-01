636fd977fb1be16a4a7dd41f1fc6e3bec7f4c65a
Rishi Ponnekanti <rishiponnekanti@gmail.com>
2020-02-01 10:53:12 -0600

    rebase

--- BeVolt/RTE/_Target_1/RTE_Components.h
+++ BeVolt/RTE/_Target_1/RTE_Components.h
@@ -16,8 +16,5 @@
  */
 #define CMSIS_device_header "stm32f4xx.h"
 
-/*  Keil::Device:Startup:2.6.3 */
-#define RTE_DEVICE_STARTUP_STM32F4XX    /* Device Startup for STM32F4 */
-
-
 #endif /* RTE_COMPONENTS_H */
+#define RTE_DEVICE_STARTUP_STM32F4XX    /* Device Startup for STM32F4 */
