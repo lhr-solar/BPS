e090f1d566af50c558a2e2d6bab8f6f74f5b599f
JimothyGreene <rishiponnekanti@gmail.com>
2020-04-28 13:53:57 -0500

    Create script to generate and display new data every second. Tested with BPS code for ADC (high/low precision current readings)

--- BeVolt/BSP/Simulator/DataGeneration/ADC.py
+++ BeVolt/BSP/Simulator/DataGeneration/ADC.py
@@ -9,6 +9,9 @@ and store the simulated data in ADC.csv to be read by BSP_ADC.c
 # path/name of file
 file = "Data/ADC.csv"
 
+# randomized current value
+random_current = 0
+
 def amps_to_adc(current, sensor):
     """
     @brief converts current in Amperes to ADC 12-bit value
@@ -40,40 +43,52 @@ def amps_to_adc(current, sensor):
 def random_adc(state, mode):
     """
     @brief generates random ADC values
-    @param state : either 'charge' or 'discharge'
+    @param state : either 'charging' or 'discharging'
     @param mode : 'low', 'normal', or 'high' ranges respective to the state
-    @return tuple (low_precision_adc, high_precision_adc)
+    @return tuple (low_precision_adc, high_precision_adc, current)
     """
-    if state == 'charge':
+    global random_current
+    if state == 'charging':
         if mode == 'low':
-            return (amps_to_adc(random.randint(-20, -15), 'l'), amps_to_adc(random.randint(-20, -15), 'h'))
+            random_current = random.randint(-20, -15)
         elif mode == 'normal':
-            return (amps_to_adc(random.randint(-15, -5), 'l'), amps_to_adc(random.randint(-15, -5), 'h'))
+            random_current = random.randint(-15, -5)
         elif mode == 'high':
-            return (amps_to_adc(random.randint(-5, 0), 'l'), amps_to_adc(random.randint(-5, 0), 'h'))
-    elif state == 'discharge':
+            random_current = random.randint(-5, 0)
+    elif state == 'discharging':
         if mode == 'low':
-            return (amps_to_adc(random.randint(0, 30), 'l'), amps_to_adc(random.randint(0, 30), 'h'))
+            random_current = random.randint(0, 30)
         elif mode == 'normal':
-            return (amps_to_adc(random.randint(30, 50), 'l'), amps_to_adc(random.randint(30, 50), 'h'))
+            random_current = random.randint(30, 50)
         elif mode == 'high':
-            return (amps_to_adc(random.randint(50, 100), 'l'), amps_to_adc(random.randint(50, 100), 'h'))
+            random_current = random.randint(50, 100)
+    return (amps_to_adc(random_current, 'l'), amps_to_adc(random_current, 'h'))
     
 
-def adc_generate(state, mode):
+def generate(state, mode):
     """
     @brief create csv file with ADC data
         Function called by simulate.py
-    @param state : either 'charge' or 'discharge'
+    @param state : either 'charging' or 'discharging'
     @param mode : 'low', 'normal', or 'high' ranges respective to the state
+    @return correct current value
     """
     with open(file, 'w') as csvfile:
         csvwriter = csv.writer(csvfile)
         csvwriter.writerow(list(random_adc(state, mode)))
 
 
-def main():
-    adc_generate('discharge', 'normal')
-
-if __name__ == '__main__':
-    main()
+def read():
+    """
+    @brief read csv file with ADC data
+        Function called by simulate.py
+    @return list of ADC data & Current data
+        Format: [(low_precision_adc, high_precision_adc), current in Amps]
+    """
+    values = []
+    with open(file, 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        for row in csvreader:
+            values.append(row)
+    values.append(random_current)
+    return values
