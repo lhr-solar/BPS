4477c38db5f11e0ed8ad1d241e0e13e0f1fc625a
Sijin Woo <sijin.woo@utexas.edu>
2020-05-04 17:09:08 -0500

    Tests setup (#61)
    Adds an easy method to create and compile with unit tests
    
    Authored by: Sijin Woo

---
+++ BeVolt/Tests/Test_Template.c
@@ -0,0 +1,10 @@
+#include "common.h"
+#include "config.h"
+
+int main() {
+
+
+    while(1) {
+        
+    }
+}
---
+++ BeVolt/Tests/Test_Voltage.c
@@ -0,0 +1,8 @@
+#include "common.h"
+#include "config.h"
+#include "Voltage.h"
+
+
+int main() {
+
+}
