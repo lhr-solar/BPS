45af2efcda3ba29ad46d817e64aa36da64f167d7
Sijin Woo <sijin.woo@utexas.edu>
2020-05-09 14:49:08 -0500

    Adds function to format the packets the LTC6811 would usually transmit inside BSP_SPI.c

--- BeVolt/Tests/Test_BSP_SPI.c
+++ BeVolt/Tests/Test_BSP_SPI.c
@@ -1,8 +1,13 @@
 #include "common.h"
 #include "config.h"
+#include "BSP_SPI.h"
+#include "BSP_UART.h"
 
 int main() {
 
+    BSP_UART_Init();    // Initialize UART to use printf
+
+    BSP_SPI_Init();
 
     while(1) {
         
