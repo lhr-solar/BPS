3752e0a2ebf4892609e66f0ddcdc063a1e86d818
Sijin Woo <sijin.woo@utexas.edu>
2020-05-09 13:54:48 -0500

    Adds PEC calculations and half of the command ltc6811 command codes

--- BeVolt/BSP/Simulator/Src/BSP_SPI.c
+++ BeVolt/BSP/Simulator/Src/BSP_SPI.c
@@ -2,6 +2,31 @@
 
 #define CSV_SPI_BUFFER_SIZE     1024
 
+/**
+ * @brief   Command Codes for the LTC6811
+ */
+#define WRCFGA
+#define WRCFGB
+#define RDCFGA
+#define RDCFGB
+#define RDCVA
+#define RDCVB
+#define RDCVC
+#define RDCVD
+#define RDCVE
+#define RDCVF
+#define RDAUXA
+#define RDAUXB
+#define RDAUXC
+#define RDAUXD
+#define RDSTATA
+#define RDSTATB
+#define WRSCTRL
+#define WRPWM
+#define WRPSB
+#define RDSCTRL
+#define RDPWM
+
 // Path relative to the executable
 const char* file = "BSP/Simulator/DataGeneration/Data/SPI.csv";
 
@@ -12,7 +37,9 @@ static uint8_t chipSelectState = 1;     // During idle, the cs pin should be hig
 
 static char csvBuffer[CSV_SPI_BUFFER_SIZE];
 
-static void GetCommand(void);
+static void PEC15_Table_Init(void);
+static uint16_t PEC15_Calc(char *data , int len);
+static void GetLTC6811Command(void);
 
 /**
  * @brief   Initializes the SPI port connected to the LTC6820. This port communicates with the LTC6811
@@ -80,3 +107,46 @@ void BSP_SPI_SetStateCS(uint8_t state) {
 /**
  * @brief   PRIVATE FUNCTIONS
  */
+
+
+
+
+/************************************
+Copyright 2012 Linear Technology Corp. (LTC)
+Permission to freely use, copy, modify, and distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies:
+THIS SOFTWARE IS PROVIDED “AS IS” AND LTC DISCLAIMS ALL WARRANTIES
+INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO
+EVENT SHALL LTC BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM ANY USE OF SAME, INCLUDING
+ANY LOSS OF USE OR DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
+OR OTHER TORTUOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.
+***********************************************************/
+static int16_t pec15Table[256];
+static int16_t CRC15_POLY = 0x4599;
+static void PEC15_Table_Init(void) {
+    for(int i = 0; i < 256; i++) {
+        int16_t remainder = i << 7;
+        for(int bit = 8; bit > 0; --bit) {
+            if(remainder & 0x4000) {
+                remainder = ((remainder << 1));
+                remainder = (remainder ^ CRC15_POLY)
+            } else {
+                remainder = ((remainder << 1));
+            }
+        }
+        pec15Table[i] = remainder&0xFFFF;
+    }
+}
+
+static uint16_t PEC15_Calc(char *data , int len) {
+    uint16_t remainder,address;
+    remainder = 16;//PEC seed
+    for(int i = 0; i < len; i++) {
+        address = ((remainder >> 7) ^ data[i]) & 0xff;//calculate PEC table address
+        remainder = (remainder << 8 ) ^ pec15Table[address];
+    }
+    return (remainder*2);//The CRC15 has a 0 in the LSB so the final value must be multiplied by 2
+}
