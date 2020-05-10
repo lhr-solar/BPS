e16a71f60c569299d58144ad196f1f01c2458039
Sijin Woo <sijin.woo@utexas.edu>
2020-05-10 15:52:05 -0500

    Adds LTC6811 command handler into BSP_SPI to simulate the BSP. Adds helper functions to easily copy and extract data. Formats the doc for BSP_SPI to make everything more readable.

--- BeVolt/Tests/Test_BSP_SPI.c
+++ BeVolt/Tests/Test_BSP_SPI.c
@@ -9,6 +9,11 @@ int main() {
 
     BSP_SPI_Init();
 
+    uint8_t data[4+NUM_MINIONS*8] = {0x00, 0x01, 0x00, 0x02};
+
+    BSP_SPI_Write(data, 4);
+    BSP_SPI_Read(data, 4+NUM_MINIONS*8);
+
     while(1) {
         
     }
