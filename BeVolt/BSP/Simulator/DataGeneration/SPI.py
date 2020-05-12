757cce8c0ecac410ecde4b8d121370c981509e58
JimothyGreene <rishiponnekanti@gmail.com>
2020-05-12 10:51:40 -0500

    Documentation & read function

--- BeVolt/BSP/Simulator/DataGeneration/SPI.py
+++ BeVolt/BSP/Simulator/DataGeneration/SPI.py
@@ -17,6 +17,10 @@ temperature_values = []     # list of 31 pairs of sensors (Celsius)
 
 
 def open_wires(battery=None):
+    """
+    @brief generate list of wire statuses (open/closed)
+    @param battery : battery object with modules
+    """
     global wires
     if battery is not None:
         wires = [1 if module.connected else 0 for module in battery.modules]
@@ -25,6 +29,11 @@ def open_wires(battery=None):
 
 
 def random_voltage(mode):
+    """
+    @brief generate randomized voltage values
+           in a specified range
+    @param mode : 'low', 'normal', or 'high' ranges
+    """
     global voltage_values
     if mode == 'low':
         voltage_values = [random.randint(250, 270) for i in range(31)]
@@ -35,11 +44,20 @@ def random_voltage(mode):
 
 
 def specific_voltage(battery):
+    """
+    @brief generate list of voltage values based on battery object
+    @param battery : battery object with module voltages
+    """
     global voltage_values
     voltage_values = [int(module.voltage*100) for module in battery.modules]
 
 
 def random_temperature(state, mode):
+    """
+    @brief generate randomized temperature values in specified ranges
+    @param state : 'charging' or 'discharging'
+    @param mode : 'low', 'normal', or 'high' ranges respective to the state
+    """
     global temperature_values
     if state == 'charging':
         if mode == 'low':
@@ -58,6 +76,13 @@ def random_temperature(state, mode):
 
 
 def generate(state, mode, battery=None):
+    """
+    @brief create csv file with voltage and temperature data
+        Function called by simulate.py
+    @param state : 'charging' or 'discharging'
+    @param mode : 'low', 'normal', or 'high' ranges respective to the state
+    @param battery : battery object with set modules
+    """
     global wires, voltage_values, temperature_values
     os.makedirs(os.path.dirname(file), exist_ok=True)
     open_wires(battery)
@@ -77,3 +102,22 @@ def generate(state, mode, battery=None):
             row.append(temperature_values[i][1])
             csvwriter.writerow(row)
             row = []
+
+
+def read():
+    """
+    @brief read csv file with wire, voltage, and temperature data
+        Function called by simulate.py
+    @return list of all data
+        Format: [wire status, voltage*100, temperature sensor 1, temperature sensor 2]
+            for each of 31 modules
+    """
+    values = []
+    os.makedirs(os.path.dirname(file), exist_ok=True)
+    with open(file, 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        for row in csvreader:
+            for i in range(len(row)):
+                row[i] = int(row[i])
+            values.append(row)
+    return values
