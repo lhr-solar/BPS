a4fab6b3775356706f266ff05b5f6c481a21b92c
JimothyGreene <rishiponnekanti@gmail.com>
2020-05-12 11:37:26 -0500

    Fix Lights.csv not existing bug on first run

--- BeVolt/BSP/Simulator/DataGeneration/Lights.py
+++ BeVolt/BSP/Simulator/DataGeneration/Lights.py
@@ -1,4 +1,5 @@
 import csv
+import os
 
 #This module will hold the statuses of the LED's on the BPS. 
 
@@ -8,8 +9,15 @@ file = "BSP/Simulator/DataGeneration/Data/Lights.csv"
 #returns integer where 9 LSB are status of 9 LED's
 def read():
     lights = []
+    os.makedirs(os.path.dirname(file), exist_ok=True)   # creates directory if not exists
+    with open(file, 'w+') as csvfile:
+        pass    # creates file if not exists
     with open(file, 'r') as csvfile: #read file
         csvreader = csv.reader(csvfile)
         for row in csvreader:
             lights.append(row)
-        return int(lights[0][0])
+        if len(lights):
+            return int(lights[0][0])
+        else:
+            return 0
+            
