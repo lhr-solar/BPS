37d87bc89af10acdfceb23c83de8dfe4ca71d29c
JimothyGreene <rishiponnekanti@gmail.com>
2020-05-12 11:29:09 -0500

    Update display to show module connection, voltage, and temperature

--- BeVolt/BSP/Simulator/DataGeneration/simulate.py
+++ BeVolt/BSP/Simulator/DataGeneration/simulate.py
@@ -25,34 +25,39 @@ def generate(battery=None):
 
 def display(battery=None):
     global stdscr
-    if battery is not None:
-        stdscr.addstr(5, 0, battery.__str__())
     # Read ADC values
     adc_values = ADC.read()
-    stdscr.addstr(0, 0, "Simulator")
-    stdscr.addstr(1, 0, "=============================")
+    stdscr.addstr(0, 10, "Battery")
+    stdscr.addstr(1, 0, "==============================")
     stdscr.addstr(2, 0, f"ADC:")
     stdscr.addstr(2, 10, f"Low Precision: {adc_values[0][0]}")
     stdscr.addstr(3, 10, f"High Precision: {adc_values[0][1]}")
     # Read Current values
     stdscr.addstr(4, 0, f"Current:")
     stdscr.addstr(4, 10, f"{adc_values[1]} A ")
+    # Read Module values
+    stdscr.addstr(0, 49, "Modules")
+    stdscr.addstr(1, 40, "===========================")
+    module_values = SPI.read()
+    for i, module in enumerate(module_values):
+        stdscr.addstr(i+2, 37, f"{i+1}")
+        stdscr.addstr(i+2, 40, f"| {'X' if module[0] else ' '} | {module[1]/100:.2f}V | {module[2]}°C | {module[3]}°C |")
     # Read LED values
+    stdscr.addstr(0, 90, "LEDs")
+    stdscr.addstr(1, 80, "=======================")
     lights = Lights.read()
-    for i in range(0, 9):
-        stdscr.addstr(i+2, 40, lights_names[i])
+    for i in range(9):
+        stdscr.addstr(i+2, 80, lights_names[i])
         if lights & (0x1<<i):
-            stdscr.addstr(i+2, 50, "[X]")
+            stdscr.addstr(i+2, 100, "[X]")
         else:
-            stdscr.addstr(i+2, 50, "[ ]")
+            stdscr.addstr(i+2, 100, "[ ]")
     stdscr.refresh()
-
 
 
 def configure():
     # Get configuration settings
     global state, mode
-    print("Welcome to the BPS Simulator")
     print("Would you like to simulate 'charging' or 'discharging'?")
     state = input()
     while state != 'charging' and state != 'discharging':
