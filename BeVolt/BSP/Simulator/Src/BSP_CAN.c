eba7d3f9e5ef872612e5b44ca860360571b0bac8
Sugam Arora <61089613+sugamarora99@users.noreply.github.com>
2020-05-28 14:33:54 -0500

    CAN simulation

--- BeVolt/BSP/Simulator/Src/BSP_CAN.c
+++ BeVolt/BSP/Simulator/Src/BSP_CAN.c
@@ -1,14 +1,27 @@
 #include "BSP_CAN.h"
+#include <stdint.h>
+#include <stdlib.h>
+
+static const char* file = "BSP/Simulator/DataGeneration/Data/CAN.csv";
+
+
 
 /**
  * @brief   Initializes the CAN module that communicates with the rest of the electrical system.
  * @param   None
  * @return  None
  */
+
+
+
 void BSP_CAN_Init(void) {
 
     // TODO: Initialize the CAN port that connects to the rest of the car.
+    FILE* fp = fopen(file, "w"); 
+    fclose(fp);
 }
+
+
 
 /**
  * @brief   Transmits the data onto the CAN bus with the specified id
@@ -17,11 +30,21 @@ void BSP_CAN_Init(void) {
  * @param   length : num of bytes of data to be transmitted. This must be <= 8 bytes or else the rest of the message is dropped.
  * @return  0 if module was unable to transmit the data onto the CAN bus. Any other value indicates data was transmitted.
  */
-uint8_t BSP_CAN_Write(uint32_t id, uint8_t data[8], uint8_t length) {
-    
-    // TODO: Transmit the data onto the CAN bus with the specified ID.
 
-    return 0;
+uint8_t BSP_CAN_Write(uint32_t id, uint8_t data[8], uint8_t length) {
+    uint32_t offset;
+    // TODO: Transmit the data onto the CAN bus with the specified ID.
+    
+
+    FILE* fp = fopen(file, "w");
+    fprintf(fp, "0x%.3x,", id);
+    for(int i = 0; i < 8; i++){
+        fprintf(fp, "%.2x", data[i]);      
+    }
+    fprintf(fp, ",%d", length);
+    fclose(fp);
+
+    return 1;
 }
 
 /**
@@ -31,11 +54,25 @@ uint8_t BSP_CAN_Write(uint32_t id, uint8_t data[8], uint8_t length) {
  * @param   id : pointer to store id of the message that was received.
  * @param   data : pointer to store data that was received. Must be 8bytes or bigger.
  * @return  0 if nothing was received so ignore id and data that was received. Any other value indicates data was received and stored.
+ * clear the message that was read from the file
  */
 uint8_t BSP_CAN_Read(uint32_t *id, uint8_t *data) {
 
     // TODO: Check if a message has been received.
     //      If message was received, then store the id and data of the message into the pointers and return a value other than 0.
+    char check;
+    FILE* fp = fopen(file, "r+");
+    fseek(fp, 6, SEEK_SET);
+    fscanf(fp, "%3x", id);
 
-    return 0;
+    fseek(fp, 19,SEEK_SET);
+    for(int i = 0; i < 8; i++){
+        fscanf(fp, "%2x", data);
+        data++;
+    }
+    fclose(fp);
+
+    fopen(file, "w");
+    fclose(fp);    
+    return 1;
 }
