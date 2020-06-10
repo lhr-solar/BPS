64f286c337dee71ba1ea08c162cfede8d801ff14
manthanand <55002301+manthanand@users.noreply.github.com>
2020-06-10 01:07:39 -0500

    Added lock functionality when accessing csv file in C and Python code.

--- BeVolt/BSP/Simulator/DataGeneration/Contactor.py
+++ BeVolt/BSP/Simulator/DataGeneration/Contactor.py
@@ -1,4 +1,6 @@
 import csv
+import fcntl
+import os
 
 #This module will read from the csv file to determine the status of the Contactor
 
@@ -7,10 +9,14 @@ file = "Data/Contactor.csv"
 
 #Reads file and returns 1 or 0, 1 is on, 0 is off
 def Read_Contactor():
+    os.makedirs(os.path.dirname(file), exist_ok=True)
     with open(file, 'r') as csvfile:
-        csvreader = csv.reader(csvfile)
-        return next(csvreader)
-        #print(next(csvreader)): used this to test function
+        fcntl.flock(csvfile.fileno(), fcntl.LOCK_EX)    # Lock file before reading
+        csvreader = csv.reader(csvfile) 
+        Contactor_State = next(csvreader)
+        fcntl.flock(csvfile.fileno(), fcntl.LOCK_UN)    # Unlock file after reading
+        return Contactor_State
+        #print(Contactor_State) #: used this to test function
 
 #Ran the function to test if it works
 #Read_Contactor()
