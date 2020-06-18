ef118363be56be8dd7eadaf385bbe493b8349e06
Sugam Arora <sugamarora@DESKTOP-EAPDOTJ.localdomain>
2020-06-18 15:06:01 -0500

    First commit

--- BeVolt/BSP/Simulator/Src/BSP_WDTimer.c
+++ BeVolt/BSP/Simulator/Src/BSP_WDTimer.c
@@ -1,5 +1,15 @@
 #include "BSP_WDTimer.h"
+#include <stdint.h>
+#include <stdlib.h>
+#include <sys/file.h>
+#include <sys/types.h>
+#include <unistd.h>
 
+
+static const char* file = "BSP/Simulator/DataGeneration/Data/WDTimer.csv";
+
+bool DidSystemReset;
+bool HasStarted;
 /**
  * @brief   Initialize the watch dog timer.
  * @param   None
@@ -8,6 +18,18 @@
 void BSP_WDTimer_Init(void) {
     // TODO: Initialize the watchdog timer but do not start it.
     //      Make the watchdog timer duration to be 1 seconds. (May change later)
+    FILE* fp = fopen(file, "w");
+    if(!fp) {
+        perror("WDTimer.csv");
+        exit(EXIT_FAILURE);
+    }
+    int fno = fileno(fp);
+    flock(fno, LOCK_EX);
+    fprintf(fp, "%d", (int)getpid()); //call some function that gets PID of simulator.out and save in in the csv
+    flock(fno, LOCK_UN);
+    fclose(fp);
+    DidSystemReset = false;
+    HasStarted = false;
 }
 
 /**
@@ -17,9 +39,68 @@ void BSP_WDTimer_Init(void) {
  */
 bool BSP_WDTimer_DidSystemReset(void) {
     // TODO: Check if the watchdog timer made the uC reset previously.
+    /* char resetValue;
+    FILE* fp = fopen(file, "r+");
+    if(!fp) {
+        perror("WDTimer.csv");
+        exit(EXIT_FAILURE);
+    }
+    int fno = fileno(fp);
+    flock(fno, LOCK_EX);
 
-    return false;
+    fseek(fp, -3, SEEK_END);
+    resetValue = fgetc(fp);
+    if(resetValue == 'b'){
+        fseek(fp, 5, SEEK_SET);
+        fprintf(fp, "  ");   //clear the 'b' and the comma before it
+        DidSystemReset = false; //reset flag
+        return true;
+    }
+    
+    flock(fno, LOCK_UN);
+    fclose(fp); */
+    if(DidSystemReset == true){
+        DidSystemReset = false;      //clear flag
+        return true;
+    }else{
+        return DidSystemReset;
+    }
 }
+
+//checks if system was reset and prints the PID in case system was reset;
+void PrintPID(void){
+    char resetValue;
+    FILE* fp = fopen(file, "r+");       //check if python script left system reset flag ('b')
+    if(!fp) {
+        perror("WDTimer.csv");
+        exit(EXIT_FAILURE);
+    }
+    int fno = fileno(fp);
+    flock(fno, LOCK_EX);
+
+    fseek(fp, -3, SEEK_END);
+    resetValue = fgetc(fp);
+    if(resetValue == 'b'){
+        DidSystemReset = true;
+    }else{
+        DidSystemReset = false;
+    }
+    flock(fno, LOCK_UN);
+    fclose(fp);
+    
+    fp = fopen(file, "w");        //print PID
+    if(!fp) {
+        perror("WDTimer.csv");
+        exit(EXIT_FAILURE);
+    }
+    fno = fileno(fp);
+    flock(fno, LOCK_EX);
+    fprintf(fp, "%d", (int)getpid()); //call some function that gets PID of simulator.out and save in in the csv
+    flock(fno, LOCK_UN);
+    fclose(fp);
+    HasStarted = false;
+}
+
 
 /**
  * @brief   Start the watchdog timer countdown. Once this is started, the BSP_WDTimer_Reset
@@ -28,7 +109,19 @@ bool BSP_WDTimer_DidSystemReset(void) {
  * @return  None
  */
 void BSP_WDTimer_Start(void) {
-    // TODO: Start the watchdog countodwn
+    // TODO: Start the watchdog countdown
+    PrintPID();
+    FILE* fp = fopen(file, "w");
+    if(!fp) {
+        perror("WDTimer.csv");
+        exit(EXIT_FAILURE);
+    }
+    int fno = fileno(fp);
+    flock(fno, LOCK_EX);
+    fprintf(fp, "%x", 0xCCCC);
+    HasStarted = true;
+    flock(fno, LOCK_UN);
+    fclose(fp);
 }
 
 /**
@@ -39,4 +132,22 @@ void BSP_WDTimer_Start(void) {
  */
 void BSP_WDTimer_Reset(void) {
     // TODO: Reset the watchdog countdown
+    //uint32_t *check;
+    if(HasStarted == true){
+        FILE* fp = fopen(file, "w");
+        if(!fp) {
+            perror("WDTimer.csv");
+            exit(EXIT_FAILURE);
+        }
+        int fno = fileno(fp);
+        flock(fno, LOCK_EX);
+        
+        fprintf(fp, "%x", 0xAAAA);
+    
+        flock(fno, LOCK_UN);
+        fclose(fp);
+    }
 }
+
+
+
---
+++ BeVolt/Tests/Test_BSP_WDTimer.c
@@ -0,0 +1,54 @@
+#include "common.h"
+#include "config.h"
+#include "BSP_WDTimer.h"
+
+int main(){
+    //bool testResetCheck = BSP_WDTimer_DidSystemReset();
+    //exit(0);
+    uint32_t delay = 150000000;
+    uint32_t secs = 40;
+    uint32_t resetCount = 3;
+    BSP_WDTimer_Start();
+    printf("pid should be saved, countdown started\n");
+    secs = 40;
+    while(secs){
+        delay = 15000000;
+        while(delay){
+            delay--;
+        }
+        secs--;
+    }
+    
+    while(1){
+
+        while(resetCount){
+            secs = 40;
+            while(secs){
+                delay = 150000000;
+                while(delay){
+                    delay--;
+                }
+                secs--;
+            }
+        
+            BSP_WDTimer_Reset();
+            printf("reset %d\n", resetCount);
+            bool checkSysReset = BSP_WDTimer_DidSystemReset();
+            printf("Has the system been reset (1 = yes, 0 = no): %d\n\n", checkSysReset);
+            resetCount--;
+        }
+        resetCount = 3;
+        printf("this process will be killed any second now...\n");
+        while(resetCount){
+            secs = 20;
+            while(secs){
+                delay = 150000000;
+                while(delay){
+                    delay--;
+                }
+                secs--;
+            }
+        }
+        exit(0);
+    } 
+}
