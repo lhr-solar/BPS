064798c648d2cb2448175069631723e026743633
manthanand <55002301+manthanand@users.noreply.github.com>
2020-06-27 15:26:39 -0500

    Voltage Library cell_asic fix (#93)
    * Created private array. Need to test with simulator.
    
    * Made private array static type variable
    
    * Fixes OpenWire bug to correctly test if copying of voltages works
    
    * Replaces include to LTC6811.h
    
    Co-authored-by: Sijin Woo <sijin.woo@utexas.edu>

--- BeVolt/BSP/Simulator/DataGeneration/simulate.py
+++ BeVolt/BSP/Simulator/DataGeneration/simulate.py
@@ -95,7 +95,7 @@ def main():
     print("Type 'start' to start BeVolt. Otherwise, you can specify the types of data to simulate.")
     print(">>", end="")
     if input() == 'start':
-        BeVolt = battery.Battery(30, 2950*434, 2900*434)
+        BeVolt = battery.Battery(30, 2950*434, 2500*434)
     else:
         BeVolt = None
         configure()
