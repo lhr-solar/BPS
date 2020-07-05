1257b5de89a61002d718a41227d06f97f0f7a7bf
Sijin Woo <sijin.woo@utexas.edu>
2020-07-05 13:13:26 -0500

    Makes strobelight.py use config.py

--- BSP/Simulator/DataGeneration/Strobelight.py
+++ BSP/Simulator/DataGeneration/Strobelight.py
@@ -1,9 +1,11 @@
 import csv
 import os
+import fcntl
+import config
 #This module holds status of strobe (on/off)
 
 #path name to file
-file = "BSP/Simulator/DataGeneration/Data/Strobe.csv"
+file = config.directory_path + config.files['Strobelight']
 
 #returns integer where 1 means strobe is on, 0 means strobe is off
 def read():
@@ -12,7 +14,9 @@ def read():
     with open(file, 'a') as csvfile:
         pass   #creates file if it doesn't exist
     with open(file, 'r') as csvfile: #read file
+        fcntl.flock(csvfile.fileno(), fcntl.LOCK_EX)    # Lock file
         csvreader = csv.reader(csvfile)
+        fcntl.flock(csvfile.fileno(), fcntl.LOCK_UN)    # Unlock file
         for row in csvreader:
             strobe.append(row)
         if len(strobe):
