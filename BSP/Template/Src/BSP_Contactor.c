9de30cb126e650314d295a35b5b0e910419d38de
ClarkPoon <43055250+ClarkPoon@users.noreply.github.com>
2020-08-02 03:13:13 -0500

    Simulator Cleanup: (#131)
    1. Changed number of EEPROM addresses from decimal into hex
    2. Added config file paths to CAN.py and Contactor.py
    3. Removed a line of code (i = 1) from CAN.py that was not doing anything, since i was set to 0 on the next line
    4. Changed the template for BSP_Contactor.c back into what it is on master (it contains the implementation for the simulator on this branch instead of a blank template)

--- BSP/Template/Src/BSP_Contactor.c
+++ BSP/Template/Src/BSP_Contactor.c
@@ -1,11 +1,6 @@
 #include "BSP_Contactor.h"
-#include "simulator_conf.h"
-#include <stdio.h>
-#include <sys/file.h>
 
-static const char* file = GET_CSV_PATH(CONTACTOR_CSV_FILE);
-
-/*
+/**
  * @brief   Initializes the GPIO pins that interfaces with the Contactor.
  *          Two GPIO pins are initialized. One as an output and one as an input.
  *          The output pin controls the state and the input pin views what state the contactor is through the
@@ -14,16 +9,8 @@ static const char* file = GET_CSV_PATH(CONTACTOR_CSV_FILE);
  * @return  None
  */
 void BSP_Contactor_Init(void) {
-    /* Hardware: Initialize two pins that will be connected to the Contactor. One must be configued as an input
-          and the other must be configued as an output.
-    Software: Create file that contains one unsigned integer, 0 means off, 1 means on
-    */
-    FILE* fp = fopen(file, "w+"); //if file doesn't exist, it is created
-    int fno = fileno(fp); //lock file
-    flock(fno, LOCK_EX);
-    fprintf(fp, "%d", 0); //write 0 to file
-    flock(fno, LOCK_UN); //unlock lock
-    fclose(fp); //close file
+    // TODO: Initialize two pins that will be connected to the Contactor. One must be configued as an input
+    //      and the other must be configued as an output.
 }
 
 /**
@@ -33,15 +20,8 @@ void BSP_Contactor_Init(void) {
  * @return  None
  */
 void BSP_Contactor_On(void) {
-    // Hardware: Set the state to high for the output pin.
+    // TODO: Set the state to high for the output pin.
     //      Use Positive Logic.
-    // Software: Set integer to 1
-    FILE* fp = fopen(file, "w"); //Open file to write
-    int fno = fileno(fp); //lock file
-    flock(fno, LOCK_EX);
-    fprintf(fp, "%d", 1); //Write 1 to file
-    flock(fno, LOCK_UN); //unlock lock
-    fclose(fp); //close file
 }
 
 /**
@@ -51,14 +31,7 @@ void BSP_Contactor_On(void) {
  * @return  None
  */
 void BSP_Contactor_Off(void) {
-    // Hardware: Set the state to low for the output pin.
-    // Software: Set integer to 0
-    FILE* fp = fopen(file, "w"); //Open file to write
-    int fno = fileno(fp); //lock file
-    flock(fno, LOCK_EX);
-    fprintf(fp, "%d", 0); //Write 0 to file
-    flock(fno, LOCK_UN); //unlock lock
-    fclose(fp); //close file
+    // TODO: Set the state to low for the output pin.
 }
 
 /**
@@ -67,16 +40,8 @@ void BSP_Contactor_Off(void) {
  * @return  0 if contactor is off/open, 1 if on/closed
  */
 bool BSP_Contactor_GetState(void) {
-    // Hardware: Return if the Contactor is on or off.
+    // TODO: Return if the Contactor is on or off.
     //      Check for negative logic. The gpio input pin can read low but the contactor could be on. Just make sure
     //      you check that your return value is the same value of what the contactor is irl.
-    // Software: Read integer stored in file
-    int ContactorState;
-    FILE* fp = fopen(file, "r"); //Open file to read
-    int fno = fileno(fp); //lock file
-    flock(fno, LOCK_EX);
-    fscanf(fp, "%d", &ContactorState); //read file
-    flock(fno, LOCK_UN); //unlock lock
-    fclose(fp); //close file
-    return ContactorState;
+    return 0;
 }
