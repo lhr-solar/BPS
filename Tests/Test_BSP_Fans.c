4d590c8fc5a640ca818f8a4919f365b821baa5c2
Manthan Upadhyaya <manthanand@gmail.com>
2020-08-29 16:19:36 -0500

    Made all functionsons compatible with 4 fans.

--- Tests/Test_BSP_Fans.c
+++ Tests/Test_BSP_Fans.c
@@ -6,16 +6,24 @@
 int main()
 {
     BSP_Fans_Init();
-    printf("Fans.csv created");
-    int x = 0;
-   /* BSP_Fans_Set(70);
-    printf("%i", BSP_Fans_GetDuty());
-    BSP_Fans_Set(100);
-    printf("%i", BSP_Fans_GetDuty());
-    BSP_Fans_Set(30);
-    printf("%i", BSP_Fans_GetDuty());
-    BSP_Fans_Set(4);
-    printf("%i", BSP_Fans_GetDuty());
-    BSP_Fans_Off();
-    printf("%i", BSP_Fans_GetDuty());*/
+    printf("Fans.csv created\n");
+    printf("%i\n", BSP_Fans_Set(2,7));
+    printf("This is test 1\n");
+    printf("The speed of fan 2 is %d\n", BSP_Fans_GetSpeed(2));
+    printf("%i\n", BSP_Fans_Set(2,9));
+    printf("This is test 2\n");
+    printf("The speed of fan 2 is %d\n", BSP_Fans_GetSpeed(2));
+    printf("%i\n", BSP_Fans_Set(5,7));
+    printf("This is test 3\n");
+    printf("The speed of fan 1 is %d\n", BSP_Fans_GetSpeed(1));
+    printf("The speed of fan 2 is %d\n", BSP_Fans_GetSpeed(2));
+    printf("The speed of fan 3 is %d\n", BSP_Fans_GetSpeed(3));
+    printf("The speed of fan 4 is %d\n", BSP_Fans_GetSpeed(4));
+    printf("%i\n", BSP_Fans_Set(5,9));
+    printf("This is test 4\n");
+    printf("The speed of fan 1 is %d\n", BSP_Fans_GetSpeed(1));
+    printf("The speed of fan 2 is %d\n", BSP_Fans_GetSpeed(2));
+    printf("The speed of fan 3 is %d\n", BSP_Fans_GetSpeed(3));
+    printf("The speed of fan 4 is %d\n", BSP_Fans_GetSpeed(4));
+    while(1);
 }
