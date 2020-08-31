6366bc39b33fa28ca214f8de2fe222aab76ad972
shannon <ax3shannonkularathna@gmail.com>
2020-08-30 21:50:09 -0400

    Build sphinx RTD theme

--- Docs/source/conf.py
+++ Docs/source/conf.py
@@ -14,7 +14,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-import sphinx_rtd_theme
+import sphinx_rtd_theme # ReadTheDocs theme. More information available at: https://sphinx-rtd-theme.readthedocs.io/en/latest/installing.html 
 
 
 # -- Project information -----------------------------------------------------
@@ -34,7 +34,7 @@ release = '0.0'
 # ones.
 extensions = [
 	"recommonmark",
-	"sphinx_rtd_theme"
+	"sphinx_rtd_theme" # RTD theme extension
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -51,7 +51,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme' # Enable RTD theme
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
