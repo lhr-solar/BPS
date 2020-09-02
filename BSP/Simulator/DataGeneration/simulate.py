e7653f09672e450dd5ba9bc9cc9a45100fad9f54
Manthan Upadhyaya <manthanand@gmail.com>
2020-09-01 23:07:55 -0500

    Added fans simulation to gui

--- BSP/Simulator/DataGeneration/simulate.py
+++ BSP/Simulator/DataGeneration/simulate.py
@@ -103,7 +103,13 @@ def display(battery=None):  #print watchdog countdown
         stdscr.addstr(11, 100, "[]", curses.color_pair(2))
     else:
         stdscr.addstr(11, 100, "[]", curses.color_pair(3))
-
+    speed = [0,1,2,3]
+    stdscr.addstr(12, 9, "Fan Speeds")
+    stdscr.addstr(13, 0, "================================")
+    stdscr.addstr(14, 0, f"Fan 1: {speed[1]}/8")
+    stdscr.addstr(14, 15, f"Fan 2: {speed[1]}/8")
+    stdscr.addstr(15, 0, f"Fan 3: {speed[2]}/8")
+    stdscr.addstr(15, 15, f"Fan 4: {speed[3]}/8")
     stdscr.refresh()
 
 
