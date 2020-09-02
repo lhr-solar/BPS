ef6090913425613565e1f0570d637f072649bc80
Manthan Upadhyaya <manthanand@gmail.com>
2020-09-02 11:17:29 -0500

    Making header files look the same

--- BSP/Inc/BSP_Fans.h
+++ BSP/Inc/BSP_Fans.h
@@ -2,8 +2,8 @@
 Controls speed of fans which cool down the battery pack.
 Uses Pins PC6,7 and PB14,15
 */
-#ifndef FANS_H__
-#define FANS_H__
+#ifndef __BSP_FANS_H
+#define __BSP_FANS_H
 
 #include "common.h"
 #include "config.h"
@@ -11,21 +11,26 @@ Uses Pins PC6,7 and PB14,15
 #include "stm32f4xx_tim.h"
 #include "stm32f4xx_rcc.h"
 
-//This function will initialize the pins and timer used to control the fans
+/**
+ * @brief   Initialize all the GPIO pins connected to each Fan
+ * @param   None
+ * @return  None
+ */
 void BSP_Fans_Init(void);
 
-/*This function will change the speed of the fans
-Inputs: Number of fan to change speed (1-4)
-        Speed of fan (0-8)
-Output: Error Status
-        0: Gave invalid fan value
-        1: Gave invalid velocity value
-*/
+/**
+ * @brief   Sets fan duty cycle
+ * @param   dutyCycle: int for duty cycle amount in range 0-8
+ *          fan: fan number whose speed should be changed
+ * @return  ErrorStatus
+ */
 ErrorStatus BSP_Fans_Set(uint8_t fan, uint32_t speed);
 
-/* Get current speed of specific fan
-Input: Fan number
-*/
+/**
+ * @brief   Get current speed of specific fan
+ * @param   fan Number
+ * @return  Current PWM duty cycle if fans
+ */
 int BSP_Fans_GetSpeed(uint8_t fan);
 
 #endif
