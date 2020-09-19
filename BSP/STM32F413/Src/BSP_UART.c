a742b762e77d7851fb868c56e7478d9f9f58e61d
Manthan Upadhyaya <manthanand@gmail.com>
2020-09-19 17:21:38 -0500

    Changed init function to also initialize UART 2

--- BSP/STM32F413/Src/BSP_UART.c
+++ BSP/STM32F413/Src/BSP_UART.c
@@ -31,10 +31,12 @@ static bool RxFifo_IsEmpty(void);
 void BSP_UART_Init(void) {
     GPIO_InitTypeDef GPIO_InitStruct = {0};
     USART_InitTypeDef UART_InitStruct = {0};
-    //Enable USART3, PC5, PB10 clocks
+    //Enable USART3, PC5, PB10, USART2, PA2, PA3 clocks
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3, ENABLE);
     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+    RCC_APB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
     //Initialize pins for USART
     GPIO_InitStruct.GPIO_Pin = GPIO_Pin_10;
     GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
@@ -44,14 +46,15 @@ void BSP_UART_Init(void) {
     GPIO_Init(GPIOB, &GPIO_InitStruct);
     GPIO_InitStruct.GPIO_Pin = GPIO_Pin_5;
     GPIO_Init(GPIOC, &GPIO_InitStruct);
-    //Enable USART2, PA2, PA3 clocks
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_GPIOA, ENABLE);
-    //Initialize pins for USART
-
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_2;
+    GPIO_Init(GPIOA, &GPIO_InitStruct);
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_3;
+    //Enable alternative function on all pins
     GPIO_PinAFConfig(GPIOB, GPIO_PinSource10, GPIO_AF_USART3);
     GPIO_PinAFConfig(GPIOC, GPIO_PinSource5, GPIO_AF_USART3);
-
+    GPIO_PinAFConfig(GPIOA, GPIO_PinSource2, GPIO_AF_USART2);
+    GPIO_PinAFConfig(GPIOA, GPIO_PinSource3, GPIO_AF_USART2);
+    //Initialize UART2 and 3
     UART_InitStruct.USART_BaudRate = 115200;
     UART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
     UART_InitStruct.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
@@ -59,17 +62,25 @@ void BSP_UART_Init(void) {
     UART_InitStruct.USART_StopBits = USART_StopBits_1;
     UART_InitStruct.USART_WordLength = USART_WordLength_8b;
     USART_Init(USART3, &UART_InitStruct);
-
+    USART_Init(USART2, &UART_InitStruct);
+    //Enable interrupts on receiving and transmitting
     USART_ITConfig(USART3, USART_IT_RXNE, ENABLE);
     USART_ITConfig(USART3, USART_IT_TC, ENABLE);
-
+    USART_ITConfig(USART2, USART_IT_RXNE, ENABLE);
+    USART_ITConfig(USART2, USART_IT_TC, ENABLE);
+    //Enable UART2 and 3
     USART_Cmd(USART3, ENABLE);
-
+    USART_Cmd(USART2, ENABLE);
+    //Enable NVIC interrupts for USART2 and 3
     NVIC_InitTypeDef NVIC_InitStructure;
   	NVIC_InitStructure.NVIC_IRQChannel = USART3_IRQn;
     NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
     NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
     NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+    NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;
     NVIC_Init(&NVIC_InitStructure);
 
     setvbuf(stdout, NULL, _IONBF, 0);
