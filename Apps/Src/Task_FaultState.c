6dce4dd2a205d39a61ea962840d9807cd27ce3af
Manthan Upadhyaya <manthanand@gmail.com>
2020-09-26 13:29:42 -0500

    Finished FaultState Task

--- Apps/Src/Task_FaultState.c
+++ Apps/Src/Task_FaultState.c
@@ -22,7 +22,6 @@ void Task_FaultState(void *p_arg) {
     // BLOCKING =====================
     // Wait until a FAULT is signaled by another task.
     OSSemPend(&Fault_Sem4, 0, OS_OPT_PEND_BLOCKING, &ts, &err);
-    
     // Turn Contactor Off
     BSP_Contactor_Off();
     // Turn Strobe Light On
