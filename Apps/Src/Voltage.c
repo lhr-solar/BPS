f748b8c920898175ac771ad149ff38fba550a26a
Sugam Arora <61089613+sugamarora99@users.noreply.github.com>
2020-10-03 11:53:47 -0500

    Voltage Mutex Initialization (#215)
    * Initial commit
    
    * Fixed function calls for pending/posting the mutex.
    
    * Changed location of pend/post functions in GetModuleMiliVoltage() and UpdateMeasurements() functions.
    
    * Removed global VoltageBuffer_Mutex variable in Tasks.c because the Voltage module is the only one that will access it. It has been moved to the Voltage module.
    
    Co-authored-by: Sugam Arora <sugamarora@DESKTOP-EAPDOTJ.localdomain>

--- Apps/Src/Voltage.c
+++ Apps/Src/Voltage.c
@@ -7,9 +7,13 @@
 #include "LTC6811.h"
 #include "config.h"
 #include <stdlib.h>
+#include "os.h"
 
 static cell_asic *Minions;
+
+static OS_MUTEX Voltage_Mutex;
 static uint16_t VoltageVal[NUM_BATTERY_MODULES]; //Voltage values gathered
+static OS_ERR err;
 /** LTC ADC measures with resolution of 4 decimal places, 
  * But we standardized to have 3 decimal places to work with
  * millivolts
@@ -18,11 +22,18 @@ static uint16_t VoltageVal[NUM_BATTERY_MODULES]; //Voltage values gathered
 /** Voltage_Init
  * Initializes all device drivers including LTC6811 and GPIO to begin Voltage Monitoring
  * @param boards LTC6811 data structure that contains the values of each register
+ * @param voltageMutex pointer to mutex, meant to pass pointer to VoltageBuffer_Mutex
  * @return SUCCESS or ERROR
  */
 ErrorStatus Voltage_Init(cell_asic *boards){
 	// Record pointer
 	Minions = boards;
+	//initialize mutex
+    OSInit(&err); 
+	OSMutexCreate(&Voltage_Mutex,
+				  "Voltage Buffer Mutex",
+				  &err
+				);
 
 	int8_t error = 0;
 	
@@ -50,7 +61,7 @@ ErrorStatus Voltage_Init(cell_asic *boards){
  */
 ErrorStatus Voltage_UpdateMeasurements(void){
 	int8_t error = 0;
-	
+	CPU_TS ts;
 	// Start Cell ADC Measurements
 	wakeup_idle(NUM_MINIONS);
 	LTC6811_adcv(ADC_CONVERSION_MODE,ADC_DCP,CELL_CH_TO_CONVERT);
@@ -61,10 +72,11 @@ ErrorStatus Voltage_UpdateMeasurements(void){
 	error = LTC6811_rdcv(0, NUM_MINIONS, Minions); // Set to read back all cell voltage registers
 	
 	//copies values from cells.c_codes to private array
+	OSMutexPend(&Voltage_Mutex, 0, OS_OPT_PEND_BLOCKING, &ts, &err);
 	for(int i = 0; i < NUM_BATTERY_MODULES; i++){
 		VoltageVal[i] = Minions[i / MAX_VOLT_SENSORS_PER_MINION_BOARD].cells.c_codes[i % MAX_VOLT_SENSORS_PER_MINION_BOARD];
 	}
-	
+	OSMutexPost(&Voltage_Mutex, OS_OPT_POST_NONE, &err);
 	if(error == 0){
 		return SUCCESS;
 	}else{
@@ -101,7 +113,6 @@ SafetyStatus Voltage_CheckStatus(void){
 SafetyStatus *Voltage_GetModulesInDanger(void){
 	static SafetyStatus checks[NUM_BATTERY_MODULES];
 	uint32_t open_wires = Voltage_GetOpenWire();
-	
 	for (int i = 0; i < NUM_BATTERY_MODULES; i++) {	
 		// Check if battery is under max voltage limit
 		if (Voltage_GetModuleMillivoltage(i) > MAX_VOLTAGE_LIMIT * MILLI_SCALING_FACTOR){
@@ -161,11 +172,11 @@ uint32_t Voltage_GetOpenWire(void){
  * @return voltage of module at specified index
  */
 uint16_t Voltage_GetModuleMillivoltage(uint8_t moduleIdx){
-    // These if statements prevents a hardfault.
+	CPU_TS ts;
+	// These if statements prevents a hardfault.
     if(moduleIdx >= NUM_BATTERY_MODULES) {
         return 0xFFFF;  // return -1 which indicates error voltage
     }
-
     // Each board will measure the same number of modules except for the last board in the daisy chain.
     // To find which minion board the battery module (moduleIdx) is assigned to, we need to
     // divide the moduleIdx by how many battery modules are assigned to each minion board
@@ -174,8 +185,10 @@ uint16_t Voltage_GetModuleMillivoltage(uint8_t moduleIdx){
     if((moduleIdx / MAX_VOLT_SENSORS_PER_MINION_BOARD) >= NUM_MINIONS) {
         return 0xFFFF;  // return -1 which indicates error voltage
     }
-
-	return VoltageVal[moduleIdx] / 10;
+	OSMutexPend(&Voltage_Mutex, 0, OS_OPT_PEND_BLOCKING, &ts, &err);
+	uint16_t ret = VoltageVal[moduleIdx] / 10;
+	OSMutexPost(&Voltage_Mutex, OS_OPT_POST_NONE, &err);
+	return ret;
 }
 
 /** Voltage_GetTotalPackVoltage
