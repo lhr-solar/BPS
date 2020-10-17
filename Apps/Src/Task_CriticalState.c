3b8c6aababcb8a722132f6778373bf2847f08eab
Manthan Upadhyaya <manthanand@gmail.com>
2020-10-17 13:52:33 -0500

    Finished CriticalState Task

--- Apps/Src/Task_CriticalState.c
+++ Apps/Src/Task_CriticalState.c
@@ -1,5 +1,7 @@
 #include "os.h"
 #include "config.h"
+#include "BSP_Contactor.h"
+#include "CANbus.h"
 #include "Tasks.h"
 
 void Task_CriticalState(void *p_arg) {
@@ -7,9 +9,17 @@ void Task_CriticalState(void *p_arg) {
 
     OS_ERR err;
     CPU_TS ts;
-
+    CANData_t Can_Data;
+    CANPayload_t Can_MSG; 
+    Can_MSG.idx = 0;
+    Can_Data.b = 1;
+    Can_Data.f = 0;
+    Can_Data.h = 0;
+    Can_Data.w = 0;
     // BLOCKING =====================
     // Wait until voltage, open wire, temperature, and current(Amperes) are all checked and safe
+    while(OSSemPend(&SafetyCheck_Sem4, 0, OS_OPT_PEND_BLOCKING, &ts, &err) != 4);
+    
     for(int32_t check = 0; check < NUM_FAULT_POINTS; check++) {
         OSSemPend(&SafetyCheck_Sem4,
                     0,
@@ -19,8 +29,9 @@ void Task_CriticalState(void *p_arg) {
     }
 
     // Turn Contactor On
-    
+    BSP_Contactor_On();
     // Push All Clear message to CAN Q
-
+    CANbus_Send(ALL_CLEAR, Can_MSG);
     // Push Contactor State message to CAN Q
+    CANbus_Send(CONTACTOR_STATE, Can_MSG);
 }
