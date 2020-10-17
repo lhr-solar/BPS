76a799b00c2d419f0ca63830f1e956dfefbca6f2
Manthan Upadhyaya <manthanand@gmail.com>
2020-10-17 14:31:32 -0500

    Deleted task at end of code and changed variable names

--- Apps/Src/Task_CriticalState.c
+++ Apps/Src/Task_CriticalState.c
@@ -9,20 +9,21 @@ void Task_CriticalState(void *p_arg) {
 
     OS_ERR err;
     CPU_TS ts;
-    CANData_t Can_Data;
-    CANPayload_t Can_MSG; 
-    Can_MSG.idx = 0;
-    Can_Data.b = 1;
-    Can_Data.f = 0;
-    Can_Data.h = 0;
-    Can_Data.w = 0;
+    CANData_t CanData;
+    CANPayload_t CanMSG; 
+    CanMSG.idx = 0;
+    CanData.b = 1;
+    CanData.f = 0;
+    CanData.h = 0;
+    CanData.w = 0;
     // BLOCKING =====================
     // Wait until voltage, open wire, temperature, and current(Amperes) are all checked and safe
     while(OSSemPend(&SafetyCheck_Sem4, 0, OS_OPT_PEND_BLOCKING, &ts, &err) != 4);
     // Turn Contactor On
     BSP_Contactor_On();
     // Push All Clear message to CAN Q
-    CANbus_Send(ALL_CLEAR, Can_MSG);
+    CANbus_Send(ALL_CLEAR, CanMSG);
     // Push Contactor State message to CAN Q
-    CANbus_Send(CONTACTOR_STATE, Can_MSG);
+    CANbus_Send(CONTACTOR_STATE, CanMSG);
+    OSTaskDel(NULL, &err); // Delete task
 }
