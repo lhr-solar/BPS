98fbb55598b7300508c312d46cf70a6bfa712c0e
Manthan Upadhyaya <manthanand@gmail.com>
2020-11-14 17:24:21 -0600

    Fixed bugs

--- Apps/Src/Voltage.c
+++ Apps/Src/Voltage.c
@@ -128,7 +128,7 @@ SafetyStatus Voltage_CheckStatus(void){
 		// VOLTAGE_LIMITS are in floating point. The LTC6811 sends the voltage data
 		// as unsigned 16-bit fixed point integers with a resolution of 0.00001
 		if(voltage > MAX_VOLTAGE_LIMIT * MILLI_SCALING_FACTOR) return OVERVOLTAGE;
-		if (Current_IsCharging){
+		if (Current_IsCharging()){
 			if(voltage < MIN_VOLTAGE_CHARGING_LIMIT * MILLI_SCALING_FACTOR) return UNDERVOLTAGE;
 		}
 		else if(voltage < MIN_VOLTAGE_LIMIT * MILLI_SCALING_FACTOR) return UNDERVOLTAGE;
