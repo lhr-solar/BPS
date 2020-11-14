155744d966b6e813e5dd475965be42404f5cb991
harshigorla <raghuharshitha12@gmail.com>
2020-11-14 16:40:17 -0600

    Sends the percentage of charge left in the battery pack to the EEPROM every 3 seconds

--- Apps/Src/Task_LogInfo.c
+++ Apps/Src/Task_LogInfo.c
@@ -1,7 +1,19 @@
 #include "os.h"
+#include "EEPROM.h"
+#include "Charge.h"
 
 void Task_LogInfo(void *p_arg) {
     (void)p_arg;
 
     OS_ERR err;
+    EEPROM_Init();
+
+    //sends percentage of charge left in the battery pack to EEPROM
+    EEPROM_LogData(DATA_SOC, Charge_GetPercent());
+
+    //1 tick every 10 millisconds, so 300 ticks for a delay of 3000ms --> 3s
+    //OS_OPT_TIME_DLY specifies relative delay, so the delay is started from the “current time + dly”.
+    OSTimeDly(300, OS_OPT_TIME_DLY, &err);
+    assertOSError(err);
+
 }
