1b5bba7ead23154ddc954385c504ee4769669aa8
Sugam Arora <61089613+sugamarora99@users.noreply.github.com>
2020-11-16 11:07:26 -0600

    Changed GetModulesInDanger Function in Voltage.c (#244)
    * Changed GetModulesInDanger() to return a struct that returns SafetyStatus arrays for the wires and modules in our system.
    
    * Changed typedef struct name to "Voltage_Info".
    
    * Removed Test_CLI.c from pull request.
    
    * Return different error messages for under and over voltage
    
    Co-authored-by: Sugam Arora <sugamarora@DESKTOP-EAPDOTJ.localdomain>
    Co-authored-by: Manthan Upadhyaya <manthanand@gmail.com>

--- Apps/Src/Voltage.c
+++ Apps/Src/Voltage.c
@@ -12,6 +12,7 @@
 #include <stdlib.h>
 #include "os.h"
 #include "Tasks.h"
+#include "Current.h"
 
 extern OS_MUTEX MinionsASIC_Mutex;
 
@@ -141,8 +142,8 @@ SafetyStatus Voltage_CheckStatus(void){
  * Each module corresponds to an index of the array of SafetyStatus
  * @return pointer to index of modules that are in danger
  */
-SafetyStatus *Voltage_GetModulesInDanger(void){
-	static SafetyStatus checks[TOTAL_VOLT_WIRES];
+Voltage_Safety Voltage_GetModulesInDanger(void){
+	static Voltage_Safety system;
 	uint32_t wires;
 	uint32_t openWireIdx = 0;
 	//put all the bits from each minion's system_open_wire variable into one variable
@@ -160,17 +161,20 @@ SafetyStatus *Voltage_GetModulesInDanger(void){
 	for (int i = 0; i < TOTAL_VOLT_WIRES; i++) {	
 		if(i < NUM_BATTERY_MODULES){
 			// Check if battery is in range of voltage limit
-			if(Voltage_GetModuleMillivoltage(i) > MAX_VOLTAGE_LIMIT * MILLI_SCALING_FACTOR || Voltage_GetModuleMillivoltage(i) < MIN_VOLTAGE_LIMIT * MILLI_SCALING_FACTOR) {
-				checks[i] = DANGER;
+			if(Voltage_GetModuleMillivoltage(i) > MAX_VOLTAGE_LIMIT * MILLI_SCALING_FACTOR) {
+				system.module_checks[i] = OVERVOLTAGE;
+			}
+			else if(Voltage_GetModuleMillivoltage(i) < MIN_VOLTAGE_LIMIT * MILLI_SCALING_FACTOR){
+				system.module_checks[i] = UNDERVOLTAGE;
 			}
 		}
 		if(openWires[i] == 1) {
-			checks[i] = DANGER;
+			system.wire_checks[i] = DANGER;
 		} else {
-			checks[i] = SAFE;
+			system.wire_checks[i] = SAFE;
 		}
 	}
-	return checks;
+	return system;
 }
 
 /** Voltage_OpenWireSummary
