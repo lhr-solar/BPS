d8ab12a27b19d5f803d549d3ae51e0a9154daedb
Sugam Arora <sugamarora@DESKTOP-EAPDOTJ.localdomain>
2020-11-21 15:55:38 -0600

    Changed the task to now use the CANMSG_t type and the CANbus_BlockAndSend() function instead of things from stm library.

--- Apps/Src/Task_CANBusConsumer.c
+++ Apps/Src/Task_CANBusConsumer.c
@@ -1,15 +1,27 @@
 #include "os.h"
+#include "Tasks.h"
+#include "BSP_CAN.h"
+#include "CANbus.h"
+
 
 void Task_CANBusConsumer(void *p_arg) {
     (void)p_arg;
 
     OS_ERR err;
+    OS_MSG_SIZE msg_size;
+    CPU_TS ts;
+    CANMSG_t *message;
 
     while(1) {
         // BLOCKING =====================
         // Wait for CAN Q to have message
-
-        // BLOCKING =====================
-        // Transmit to CAN Bus
+        message = (CANMSG_t *)OSQPend(&CANBus_MsgQ,
+                                    0,  //timeout
+                                    OS_OPT_PEND_BLOCKING,   //optimization
+                                    &msg_size,  
+                                    &ts,    //pointer to variable that will recieve timestamp of when the message was recivied.
+                                    &err);
+        assertOSError(err);
+        CANbus_BlockAndSend(message->id, message->payload);
     }
 }
