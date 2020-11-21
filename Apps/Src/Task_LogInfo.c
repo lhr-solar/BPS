4dab5370387d9231522fdfec7c55baf702c98404
ClarkPoon <clarkap26@gmail.com>
2020-11-21 14:46:03 -0600

    Fixed more issues that Sijin and I found: 1. Added while(1) loops to LogInfo and PetWDog 2. removed unnecessasry includes 3. got rid of weird question marks

--- Apps/Src/Task_LogInfo.c
+++ Apps/Src/Task_LogInfo.c
@@ -9,12 +9,13 @@ void Task_LogInfo(void *p_arg) {
     OS_ERR err;
     EEPROM_Init();
 
-    //sends percentage of charge left in the battery pack to EEPROM
-    EEPROM_LogData(DATA_SOC, Charge_GetPercent());
+    while (1){
+        //sends percentage of charge left in the battery pack to EEPROM
+        EEPROM_LogData(DATA_SOC, Charge_GetPercent());
 
-    //1 tick every 10 millisconds, so 300 ticks for a delay of 3000ms --> 3s
-    //OS_OPT_TIME_DLY specifies relative delay, so the delay is started from the �current time + dly�.
-    OSTimeDly(300, OS_OPT_TIME_DLY, &err);
-    assertOSError(err);
-
+        //1 tick every 10 millisconds, so 300 ticks for a delay of 3000ms --> 3s
+        //OS_OPT_TIME_DLY specifies relative delay, so the delay is started from the "current time + dly".
+        OSTimeDly(300, OS_OPT_TIME_DLY, &err);
+        assertOSError(err);
+    }
 }
--- Apps/Src/Task_PetWDog.c
+++ Apps/Src/Task_PetWDog.c
@@ -1,6 +1,5 @@
 #include "Tasks.h"
 #include "os.h"
-#include <stdlib.h>
 #include "BSP_WDTimer.h"
 
 void Task_PetWDog(void *p_arg) {
@@ -8,19 +7,23 @@ void Task_PetWDog(void *p_arg) {
 
     OS_ERR err;
 
-    //take WDog Mutex
-    OSMutexPend(&WDog_Mutex, 0, OS_OPT_PEND_BLOCKING, NULL, &err);
-    assertOSError(err);
+    while (1){
+        //take WDog Mutex
+        OSMutexPend(&WDog_Mutex, 0, OS_OPT_PEND_BLOCKING, NULL, &err);
+        assertOSError(err);
 
-    // If WDog_BitMap is all set:
-    // Reset watchdog timer
-    // The three least significant bits of WDog_BitMap will be set if:
-    // temp, voltage, current tasks are set --> value = 7
-     if (WDog_BitMap == 7) {     
-         BSP_WDTimer_Reset();
-     }
+        // If WDog_BitMap is all set:
+        // Reset watchdog timer
+        // The three least significant bits of WDog_BitMap will be set if:
+        // temp, voltage, current tasks are set --> value = 7
+        if (WDog_BitMap == 7) {     
+             BSP_WDTimer_Reset();
+        }
    
-    //release WDog Mutex
-    OSMutexPost(&WDog_Mutex, OS_OPT_POST_NONE, &err);
-    assertOSError(err);
-}
+        //release WDog Mutex
+        OSMutexPost(&WDog_Mutex, OS_OPT_POST_NONE, &err);
+        assertOSError(err);
+
+        OSTimeDly(40, OS_OPT_TIME_DLY, &err);
+        assertOSError(err);
+    }
