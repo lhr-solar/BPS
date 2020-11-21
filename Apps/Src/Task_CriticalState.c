262d495f29e8ba1d02d94960e04340e4812fa1a5
Sijin Woo <sijin.woo@utexas.edu>
2020-11-21 12:27:52 -0800

    Adds startup code to allow admin privledges when charging

--- Apps/Src/Task_CriticalState.c
+++ Apps/Src/Task_CriticalState.c
@@ -3,6 +3,11 @@
 #include "BSP_Contactor.h"
 #include "CANbus.h"
 #include "Tasks.h"
+#include "BSP_UART.h"
+
+bool AdminOverride = false;
+
+static char command[COMMAND_SIZE];
 
 void Task_CriticalState(void *p_arg) {
     (void)p_arg;
@@ -17,6 +22,21 @@ void Task_CriticalState(void *p_arg) {
     CanData.f = 0;
     CanData.h = 0;
     CanData.w = 0;
+
+    Voltage_UpdateMeasurements();
+	SafetyStatus voltStatus = Voltage_CheckStatus();
+	if(voltStatus == UNDERVOLTAGE) {
+		printf("Do you need to charge the batteries? (y/n)\n\r>> ");
+		uint32_t wait = 0;
+		while(wait < STARTUP_WAIT_TIME) {
+			if(BSP_UART_ReadLine(command, UART_USB)) {
+				AdminOverride = command[0] == 'y' ? true : false;
+				break;
+			}
+			wait++;
+		}
+	}
+
     // BLOCKING =====================
     // Wait until voltage, open wire, temperature, and current(Amperes) are all checked and safe
     while(OSSemPend(&SafetyCheck_Sem4, 0, OS_OPT_PEND_BLOCKING, &ts, &err) != 4);
