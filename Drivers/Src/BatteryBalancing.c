64d4e9ab0cfa13f7c06eeb0a3873e81020157d97
Sugam Arora <61089613+sugamarora99@users.noreply.github.com>
2020-12-12 10:38:09 -0600

    Battery Balancing API (#134)
    * Made 'PLL' and 'CAN' valid menu options regardless of if the simulator is using a battery object or by specifying a configuration
    
    * Added Chase's suggestions:
    1. created menu with option to use numbers to select options
    2. Rearranged code in menu so that redundant code was removed
    
    * Created BatteryBalancing.c and .h files that have almost the same code from the "BatteryBalancing" branch. Changed the total_ic parameter from 0 to 4 in the LTC6811_set_discharge() call in setDischarge() because passing a 0 results in the function doing nothing. Changed the | to a ^ in ClearDischargeBit because 1^1 = 0.
    
    * Added comments for functions in header file. Deleted  XOR and used ~ instead in ClearDischargeBit(). Changed total_ic parameter in all LTC681x function calls to 4 instead of 1 because our system has 4 ICs.
    
    * Changed API function names to have "Balancing__()". Made Balancing_Init() a static helper function that Balancing_Balance() calls. Removed globals and all printf() statements. Updated documentation in header file.
    
    * Fixed build errors and made changes requested
    
    * Removed RTOS and Docs folder
    
    * Removed helper functions from the header file. Changed LTC library function call to use NUM_MINIONS macro. Added function descriptions to source file. Battery Balancing functions no longer update voltage measurements. Added "return values" back to read() function from SPI.py
    
    * Fixed compiler errors and warnings
    
    Co-authored-by: ClarkPoon <clarkap26@gmail.com>
    Co-authored-by: ClarkPoon <43055250+ClarkPoon@users.noreply.github.com>
    Co-authored-by: Sugam Arora <sugamarora@DESKTOP-EAPDOTJ.localdomain>
    Co-authored-by: Manthan Upadhyaya <manthanand@gmail.com>

---
+++ Drivers/Src/BatteryBalancing.c
@@ -0,0 +1,108 @@
+
+#include "LTC6811.h"
+#include "Voltage.h"
+#include "BatteryBalancing.h"
+#include "config.h"
+#include "Current.h"
+
+#define CHARGING_TOLERANCE 0
+
+static void Balancing_ClearDischargeBit(int Cell, uint8_t total_ic, cell_asic *ic);
+static void Balancing_GetICNumber(uint8_t i, uint8_t* ICNumber, uint8_t* ModuleNumber);
+static void Balancing_GetICNumber(uint8_t i, uint8_t* ICNumber, uint8_t* ModuleNumber);
+static void Balancing_SetDischargeBit(uint8_t module, cell_asic ic[]);
+
+/**
+ * @brief   Finds lowest voltage amongst the 31 modules. Meant to be called only in Balancing_Balance()
+ * @param   None
+ * @return  the  minimum voltage
+ */
+static uint16_t GetMinimumVoltage(void) {
+	uint16_t minVoltage = 0;
+	minVoltage = Voltage_GetModuleMillivoltage(0);
+	for (uint8_t i = 1; i < NUM_BATTERY_MODULES; i++){
+		if(Voltage_GetModuleMillivoltage(i) < minVoltage) {
+			minVoltage = Voltage_GetModuleMillivoltage(i);//Find minimum voltage
+		}
+	}
+	return minVoltage;
+}
+
+/**
+ * @brief   Loops through all 31 modules, sets discharge bits for any module if its voltage is too high, and clears discharge bits for any modules with voltages that are too low
+ * @param   Minions array of the ICs that the modules are connected to
+ * @return  None
+ */	
+void Balancing_Balance(cell_asic Minions[]){ 
+	uint16_t lowest = GetMinimumVoltage(); //get lowest voltage 
+	for (uint8_t k = 0; k < NUM_BATTERY_MODULES; k++) {
+		uint16_t voltage = Voltage_GetModuleMillivoltage(k);	
+		if (voltage > lowest + CHARGING_TOLERANCE) {	
+			Balancing_SetDischargeBit(k, Minions);	
+		}
+		else {	//Clear discharge bit of module if it reaches minimum
+			uint8_t ICIndex;
+			uint8_t MNumber;
+			Balancing_GetICNumber(k, &ICIndex, &MNumber);	
+			Balancing_ClearDischargeBit(MNumber, NUM_MINIONS, &Minions[ICIndex]);	
+		}
+	}
+}
+
+/**
+ * @brief   Clears the discharge bit of the desired module
+ * @param   Cell cell that will stop discharging
+ * @param   total_ic total number of ICs in the system
+ * @param   ic array of ICs in the system
+ * @return  None
+ */
+static void Balancing_ClearDischargeBit(int Cell, uint8_t total_ic, cell_asic *ic){
+	for(int i=0; i<total_ic; i++){
+		if((Cell<9)&& (Cell!=0)){
+			ic[i].config.tx_data[4] = ic[i].config.tx_data[4] & ~(1<<(Cell-1));
+		}
+    	else if(Cell < 13){
+			ic[i].config.tx_data[5] = ic[i].config.tx_data[5] & ~(1<<(Cell-9));
+		}
+		else{
+			break;
+		}
+  	}
+}
+
+/**
+ * @brief   Recieves module number out of 31 and stores the module number and its IC number in two buffers
+ * @param   i module number out of 31
+ * @param   ICNumber buffer for IC number
+ * @param   ModuleNumber buffer for module number
+ * @return  None
+ */
+static void Balancing_GetICNumber(uint8_t i, uint8_t* ICNumber, uint8_t* ModuleNumber) {
+	uint8_t total = 0;
+	uint8_t NUM_MODULES_PER_MINION[4] = {8,8,8,7};
+	for(int m = 0; m < NUM_MINIONS; m++) {
+		total += NUM_MODULES_PER_MINION[m];
+		if(i < total) {
+			*ICNumber = m;
+			*ModuleNumber = i - total;
+			return;
+		}
+	}
+}
+
+/**
+ * @brief   Sets the discharge bit of the desired module
+ * @param   module cell that will discharge
+ * @param   ic array of ICs in the system
+ * @return  None
+ */
+static void Balancing_SetDischargeBit(uint8_t module, cell_asic ic[]) { 
+	uint8_t ICNumber = 0; 
+	uint8_t ModuleNumber = 0;
+	Balancing_GetICNumber(module, &ICNumber, &ModuleNumber);//Get IC and ModuleInIC number
+	LTC681x_rdcfg(NUM_MINIONS, ic);
+	
+	LTC6811_set_discharge(ModuleNumber, NUM_MINIONS, &ic[ICNumber]); //Set discharge bit
+	LTC681x_wrcfg(NUM_MINIONS, ic);	
+	LTC681x_rdcfg(NUM_MINIONS, ic);
+}
