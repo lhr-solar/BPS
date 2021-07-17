79177ebce41898e8ff0deae7346361017032ea46
ClarkPoon <clarkap26@gmail.com>
2021-07-17 11:19:12 -0700

    removed steps that used test board and UART

--- Tests/Test_CriticalState_2.c
+++ Tests/Test_CriticalState_2.c
@@ -19,10 +19,8 @@ CPU_STK Task2_Stk[256];
  * 
  * 1. Build the BPS code for the stm32f413 for this test file (see README.md)
  * 2. Flash the BPS (see README.md)
- * 3. Connect the BPS UART USB to your laptop and open PuTTY (or comparable)
- * 4. Connect the BPS test board
- * 5. Run the code in this file. NOTE: This code includes a heartbeat.
- * 6. Make sure the contactor stays open. Use an ohmmeter to check the resistance across the contactor's terminals to determine the state of the contactor (this resistance should be very high/open circuit).
+ * 3. Run the code in this file. NOTE: This code includes a heartbeat.
+ * 4. Make sure the contactor stays open. Use an ohmmeter to check the resistance across the contactor's terminals to determine the state of the contactor (this resistance should be very high/open circuit).
 ****************************************************************************/
 
 
