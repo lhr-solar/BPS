2f5c87d69c46a42cd1577b900245ca3100c4a8dd
ClarkPoon <clarkap26@gmail.com>
2021-07-17 18:58:39 -0700

    fixed compile error

--- Tests/Test_Amperes_Task.c
+++ Tests/Test_Amperes_Task.c
@@ -36,8 +36,7 @@ void Task1(void *p_arg){
 	OS_CPU_SysTickInit(SystemCoreClock / (CPU_INT32U) OSCfg_TickRate_Hz);
 
     OS_ERR err;
-
-    OS_ERR err;
+    
     OSSemCreate(&Fault_Sem4,
                 "Fault/Tripped Semaphore",
                 0,
