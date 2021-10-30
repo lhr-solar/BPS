09515d8e81fd96468e6e767185fd4c98052829f7
Clark Poon <clarkap26@gmail.com>
2021-10-30 13:56:11 -0500

    fixed ADS7042 driver

--- Drivers/Src/ADS7042.c
+++ Drivers/Src/ADS7042.c
@@ -2,7 +2,34 @@
 #include "BSP_SPI.h"
 #include "os.h"
 #include "Tasks.h"
+#include "BSP_PLL.h"
 
+// Helper functions copied from LTC681x.c
+
+static void cs_set(uint8_t state){
+	BSP_SPI_SetStateCS(spi_ads7042, state);
+}
+
+static void ADS7042_delay_u(uint16_t micro)
+{
+  uint32_t delay = BSP_PLL_GetSystemClock() / 1000000;
+	for(uint32_t i = 0; i < micro; i++)
+	{
+		for(uint32_t j = 0; j < delay; j++);
+	}
+}
+
+//Generic wakeup command to wake the LTC6813 from sleep
+static void ADS7042_wakeup_sleep(uint8_t total_ic)
+{
+  for (int i =0; i<total_ic; i++)
+  {
+    cs_set(0);
+    ADS7042_delay_u(500); // Guarantees the LTC6813 will be in standby
+    cs_set(1);
+    ADS7042_delay_u(150);
+  }
+}
 
 /* Initialize communication ADS7042
  * and run startup offset calibration
@@ -22,6 +49,8 @@ void ADS7042_Init(bsp_os_t spi_os) {
  */
 uint16_t ADS7042_Read() {
     uint8_t rxdata[2];
+
+    ADS7042_wakeup_sleep(1);
 
     BSP_SPI_SetStateCS(spi_ads7042, 0);
     BSP_SPI_Read(spi_ads7042, rxdata, 2);
@@ -47,6 +76,6 @@ int32_t ADS7042_GetCurrent() {
     const int32_t OFFSET = 0x0800;
     const int32_t NUMERATOR = 7324;
     const int32_t DENOMINATOR = 100;
-    int32_t milliamps = ((reading - OFFSET) * NUMERATOR) / DENOMINATOR;
+    int32_t milliamps = -1 * ((reading - OFFSET) * NUMERATOR) / DENOMINATOR;    // during testing, the gain seems to be inverted
     return milliamps;
 }
