5b6952c2b1852dccc02a9a557dd48cbb824bd3f8
bps <manthanand@gmail.com>
2021-11-20 15:05:38 -0600

    Fixed whitespaces

--- Tests/Test_VoltTempMonitorTask.c
+++ Tests/Test_VoltTempMonitorTask.c
@@ -90,34 +90,34 @@ void Task1(void *p_arg){
             &err);					// return err code
 
     //3
-        OSTaskCreate(&PetWDog_TCB,				// TCB
-				"TASK_PETWDOG_PRIO",	// Task Name (String)
-				Task_PetWDog,				// Task function pointer
-				(void *)0,				// Task function args
-				TASK_PETWDOG_PRIO,			// Priority
-				PetWDog_Stk,				// Stack
-				WATERMARK_STACK_LIMIT,	// Watermark limit for debugging
-				TASK_PETWDOG_STACK_SIZE,		// Stack size
-				0,						// Queue size (not needed)
-				10,						// Time quanta (time slice) 10 ticks
-				(void *)0,				// Extension pointer (not needed)
-				OS_OPT_TASK_STK_CHK | OS_OPT_TASK_SAVE_FP,	// Options
-				&err);					// return err code
+    OSTaskCreate(&PetWDog_TCB,				// TCB
+			"TASK_PETWDOG_PRIO",	// Task Name (String)
+			Task_PetWDog,				// Task function pointer
+			(void *)0,				// Task function args
+			TASK_PETWDOG_PRIO,			// Priority
+			PetWDog_Stk,				// Stack
+			WATERMARK_STACK_LIMIT,	// Watermark limit for debugging
+			TASK_PETWDOG_STACK_SIZE,		// Stack size
+			0,						// Queue size (not needed)
+			10,						// Time quanta (time slice) 10 ticks
+			(void *)0,				// Extension pointer (not needed)
+			OS_OPT_TASK_STK_CHK | OS_OPT_TASK_SAVE_FP,	// Options
+			&err);					// return err code
     
     // Spawn Task_VoltTempMonitor with PRIO 4
     OSTaskCreate(&VoltTempMonitor_TCB,				// TCB
-				"TASK_VOLT_TEMP_MONITOR_PRIO",	// Task Name (String)
-				Task_VoltTempMonitor,				// Task function pointer
-				(void *)0,				// Task function args
-				TASK_VOLT_TEMP_MONITOR_PRIO,			// Priority
-				VoltTempMonitor_Stk,				// Stack
-				WATERMARK_STACK_LIMIT,	// Watermark limit for debugging
-				TASK_VOLT_TEMP_MONITOR_STACK_SIZE,		// Stack size
-				0,						// Queue size (not needed)
-				10,						// Time quanta (time slice) 10 ticks
-				(void *)0,				// Extension pointer (not needed)
-				OS_OPT_TASK_STK_CHK | OS_OPT_TASK_SAVE_FP,	// Options
-				&err);					// return err code
+			"TASK_VOLT_TEMP_MONITOR_PRIO",	// Task Name (String)
+			Task_VoltTempMonitor,				// Task function pointer
+			(void *)0,				// Task function args
+			TASK_VOLT_TEMP_MONITOR_PRIO,			// Priority
+			VoltTempMonitor_Stk,				// Stack
+			WATERMARK_STACK_LIMIT,	// Watermark limit for debugging
+			TASK_VOLT_TEMP_MONITOR_STACK_SIZE,		// Stack size
+			0,						// Queue size (not needed)
+			10,						// Time quanta (time slice) 10 ticks
+			(void *)0,				// Extension pointer (not needed)
+			OS_OPT_TASK_STK_CHK | OS_OPT_TASK_SAVE_FP,	// Options
+			&err);					// return err code
 
     // Spawn CANBUS Consumer, PRIO 7
     OSTaskCreate(&CANBusConsumer_TCB,				// TCB
