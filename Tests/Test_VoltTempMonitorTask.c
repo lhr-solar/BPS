33959f6f6ae1f41819c3e9cca00d74ff27117526
bps <manthanand@gmail.com>
2021-11-20 12:49:28 -0600

    Added WDog task and set bitmaps for amperes and batterybalancing task

--- Tests/Test_VoltTempMonitorTask.c
+++ Tests/Test_VoltTempMonitorTask.c
@@ -29,10 +29,12 @@
 OS_TCB Task1_TCB;
 CPU_STK Task1_Stk[DEFAULT_STACK_SIZE];
 
+OS_TCB Task2_TCB;
+CPU_STK Task2_Stk[DEFAULT_STACK_SIZE];
 
 // Initialization task for this test
 void Task1(void *p_arg){
-	OS_CPU_SysTickInit(SystemCoreClock / (CPU_INT32U) OSCfg_TickRate_Hz);
+    OS_CPU_SysTickInit(SystemCoreClock / (CPU_INT32U) OSCfg_TickRate_Hz);
 
     OS_ERR err;
     
@@ -87,6 +89,21 @@ void Task1(void *p_arg){
             OS_OPT_TASK_STK_CHK | OS_OPT_TASK_SAVE_FP,	// Options
             &err);					// return err code
 
+    //3
+        OSTaskCreate(&PetWDog_TCB,				// TCB
+				"TASK_PETWDOG_PRIO",	// Task Name (String)
+				Task_PetWDog,				// Task function pointer
+				(void *)0,				// Task function args
+				TASK_PETWDOG_PRIO,			// Priority
+				PetWDog_Stk,				// Stack
+				WATERMARK_STACK_LIMIT,	// Watermark limit for debugging
+				TASK_PETWDOG_STACK_SIZE,		// Stack size
+				0,						// Queue size (not needed)
+				10,						// Time quanta (time slice) 10 ticks
+				(void *)0,				// Extension pointer (not needed)
+				OS_OPT_TASK_STK_CHK | OS_OPT_TASK_SAVE_FP,	// Options
+				&err);					// return err code
+    
     // Spawn Task_VoltTempMonitor with PRIO 4
     OSTaskCreate(&VoltTempMonitor_TCB,				// TCB
 				"TASK_VOLT_TEMP_MONITOR_PRIO",	// Task Name (String)
@@ -121,18 +138,18 @@ void Task1(void *p_arg){
     CAN_Queue_Init();
     assertOSError(err);
 
-    // get contactor to close without temperature or voltage readings
-    OSSemPost(&SafetyCheck_Sem4,
-                OS_OPT_POST_1,
-                &err);
-    assertOSError(err);
-    OSSemPost(&SafetyCheck_Sem4,
-                OS_OPT_POST_1,
-                &err);
-    assertOSError(err);
-
 	//delete task
 	OSTaskDel(NULL, &err); // Delete task
+}
+
+//Task to prevent watchdog from tripping
+void Task2(void *p_arg){
+        OS_ERR err;
+        WDog_BitMap |= WD_AMPERES;
+        WDog_BitMap |= WD_BALANCING;
+        //delay of 100ms
+        OSTimeDly(10, OS_OPT_TIME_DLY, &err);
+        assertOSError(err);
 }
 
 // Similar to the production code main. Does not check watchdog or mess with contactor 
@@ -149,6 +166,21 @@ int main(void) {
                 (void *)0,
                 1,
                 Task1_Stk,
+                16,
+                256,
+                0,
+                0,
+                (void *)0,
+                OS_OPT_TASK_SAVE_FP | OS_OPT_TASK_STK_CHK,
+                &err);
+    assertOSError(err);
+
+    OSTaskCreate(&Task2_TCB,
+                "Task 2",
+                Task2,
+                (void *)0,
+                1,
+                Task2_Stk,
                 16,
                 256,
                 0,
