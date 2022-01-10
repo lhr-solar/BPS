12be9f20e8ca57c5709be02ad8f974290c681396
Clark Poon <clarkap26@gmail.com>
2022-01-10 00:01:25 -0600

    added sudo and ulimit to hopefully allow the github server to run the simulator

--- Continuous_Integration/CANbus/Test_CANbus.sh
+++ Continuous_Integration/CANbus/Test_CANbus.sh
@@ -2,6 +2,8 @@
 
 # meant to be run from the BPS directory
 
+ulimit -r unlimited
+
 ./bps-simulator.out
 
 diff ./BSP/Simulator/DataGeneration/Data/CAN.csv ./Continuous_Integration/CANbus/expected.csv
