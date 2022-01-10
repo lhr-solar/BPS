b62d9149630e00aa6939a94e4d4f9d140a510e0b
Clark Poon <clarkap26@gmail.com>
2022-01-10 15:55:39 -0600

    added some debugging

--- Continuous_Integration/CANbus/Test_CANbus.sh
+++ Continuous_Integration/CANbus/Test_CANbus.sh
@@ -1,10 +1,12 @@
-#! /bin/bash 
+#! /bin/bash -x
 
 # meant to be run from the BPS directory
 
 ulimit -r unlimited
 
 ./bps-simulator.out
+
+ls BSP/Simulator/DataGeneration/Data
 
 diff ./BSP/Simulator/DataGeneration/Data/CAN.csv ./Continuous_Integration/CANbus/expected.csv
 
