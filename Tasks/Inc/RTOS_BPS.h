20fbacb68615d70e6fd7091da22d3bd0daf6ff75
Siddhant779 <70386749+Siddhant779@users.noreply.github.com>
2022-09-03 18:19:34 -0500

    Rtos library (#473)
    * made the library files for RTOS
    
    * made basic structure of RTOS library
    
    * changed the names of the file
    
    * changes to file and function name
    
    * added description line
    
    * made formatting changes

---
+++ Tasks/Inc/RTOS_BPS.h
@@ -0,0 +1,56 @@
+/* Copyright (c) 2022 UT Longhorn Racing Solar 
+RTOS_BPS library includes all the wrapper functions for a functioning RTOS - needed for cleaner code and portability
+*/
+
+//need to add the proper #include files 
+
+#ifndef RTOS_BPS_H
+#define RTOS_BPS_H
+
+//#include
+
+
+/**
+ * @brief   
+ * @param   
+ * @return  
+ */
+void RTOS_BPS_SemPend(void);
+
+/**
+ * @brief   
+ * @param   
+ * @return  
+ */
+void RTOS_BPS_SemPost(void);
+
+/**
+ * @brief   
+ * @param   
+ * @return  
+ */
+void RTOS_BPS_MutexPend(void);
+
+/**
+ * @brief   
+ * @param   
+ * @return  
+ */
+void RTOS_BPS_MutexPost(void);
+
+/**
+ * @brief   
+ * @param   
+ * @return  
+ */
+void RTOS_BPS_TaskCreate(void);
+
+/**
+ * @brief   
+ * @param   
+ * @return  
+ */
+void RTOS_BPS_TimeDelay(void);
+
+
+#endif 
---
+++ Tasks/Src/RTOS_BPS.c
@@ -0,0 +1,24 @@
+/* Copyright (c) 2022 UT Longhorn Racing Solar 
+RTOS_BPS library includes all the wrapper functions for a functioning RTOS - needed for cleaner code and portability
+*/
+
+#include "RTOS_BPS.h"
+
+void RTOS_BPS_SemPend(void){ 
+
+} 
+void RTOS_BPS_SemPost(void){
+
+}
+void RTOS_BPS_MutexPend(void){
+
+}
+void RTOS_BPS_MutexPost(void){
+
+}
+void RTOS_BPS_TaskCreate(void){
+
+}
+void RTOS_BPS_TimeDelay(void){
+
+}
