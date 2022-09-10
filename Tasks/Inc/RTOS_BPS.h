aab610c4cb43a54ae64e1ed655dd0cfef97a4fda
connorl309 <cleu309fw@gmail.com>
2022-09-10 14:49:08 -0500

    Writing RTOS_BPS_MutexPost wrapper functions, researching FreeRTOS

--- Tasks/Inc/RTOS_BPS.h
+++ Tasks/Inc/RTOS_BPS.h
@@ -3,23 +3,34 @@ RTOS_BPS library includes all the wrapper functions for a functioning RTOS - nee
 */
 
 //need to add the proper #include files 
+#include "os.h" // for RTOS stuff
+#include "Tasks.h" // for OS errors
 
 #ifndef RTOS_BPS_H
 #define RTOS_BPS_H
 
-//#include
+// Custom typedefs and options for mutex/semaphore functionality
+// ONLY change the first column in the TypeDef depending on the RTOS used
+
+// Currently using Micrium
+typedef OS_MUTEX    BPS_OS_MUTEX;
+typedef OS_SEM      BPS_OS_SEM;
+typedef OS_OPT      BPS_OS_OPT;
+typedef OS_ERR      BPS_OS_ERR;
+typedef OS_SEM      BPS_OS_SEM;
+typedef OS_TCB      BPS_OS_TCB;
 
 
 /**
- * @brief   
- * @param   
- * @return  
+ * @brief
+ * @param
+ * @return
  */
 void RTOS_BPS_SemPend(void);
 
 /**
- * @brief   
- * @param   
+ * @brief
+ * @param
  * @return  
  */
 void RTOS_BPS_SemPost(void);
@@ -32,11 +43,12 @@ void RTOS_BPS_SemPost(void);
 void RTOS_BPS_MutexPend(void);
 
 /**
- * @brief   
- * @param   
- * @return  
+ * @brief   Posts the specified Mutex
+ * @param   *mutex - pointer to the specified RTOS Mutex object
+ * @param   options - a parameter which determines what kind of Post MutexPost performs
+ * @return  BPS_OS_ERR value
  */
-void RTOS_BPS_MutexPost(void);
+BPS_OS_ERR RTOS_BPS_MutexPost(BPS_OS_MUTEX* mutex, BPS_OS_OPT options);
 
 /**
  * @brief   
--- Tasks/Src/RTOS_BPS.c
+++ Tasks/Src/RTOS_BPS.c
@@ -4,17 +4,31 @@ RTOS_BPS library includes all the wrapper functions for a functioning RTOS - nee
 
 #include "RTOS_BPS.h"
 
-void RTOS_BPS_SemPend(void){ 
+void RTOS_BPS_SemPend() {
 
 } 
-void RTOS_BPS_SemPost(void){
+void RTOS_BPS_SemPost() {
 
 }
 void RTOS_BPS_MutexPend(void){
 
 }
-void RTOS_BPS_MutexPost(void){
-
+// Connor Leu
+// Post == Give
+BPS_OS_ERR RTOS_BPS_MutexPost(BPS_OS_MUTEX* mutex, BPS_OS_OPT options) {
+    BPS_OS_ERR err;
+    OSMutexPost(mutex, options, &err); // calls MutexPost with all specified params
+    assertOSError(err);
+    return err;
+    /**
+     * FreeRTOS only uses ONE parameter for its MutexPost, a SemaphoreHandle_t object (no ptr)
+     * CANNOT be used in an ISR.
+     * xSemaphoreGive() returns pdTRUE if the semaphore is released, pdFALSE if there is some error.
+     * 
+     * Blocking vs Nonblocking appears to come from https://www.freertos.org/xQueueSelectFromSet.html, and
+     * https://www.freertos.org/xQueueCreateSet.html
+     * which is a separate structure used and not just an option passed into the pend/post (take/give) functions
+    */ 
 }
 void RTOS_BPS_TaskCreate(void){
 
