9c3dc287184cc12ba9904b22e7eb1a89aac2b6bd
Raymond Jiang <raymondjiang2014@gmail.com>
2022-09-20 19:41:33 +0000

    initializes PLL at 80MHz, default: 16MHz (w/o init)

--- BSP/Simulator_V2/Src/BSP_PLL.c
+++ BSP/Simulator_V2/Src/BSP_PLL.c
@@ -4,13 +4,15 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+static uint32_t SystemCoreClock = 16000000;
 /**
  * @brief   Initialize the PLL so the system core frequency runs at your preferred frequency.
  * @param   None
  * @return  None
  */
 void BSP_PLL_Init(void) {
-    // TODO
+    SystemCoreClock = 80000000;
+    Simulator_log("Initialized PLL",15);
 }
 
 /**
@@ -19,5 +21,5 @@ void BSP_PLL_Init(void) {
  * @return  System core clock frequency in Hz
  */
 uint32_t BSP_PLL_GetSystemClock(void) {
-    // TODO
+    return SystemCoreClock;
 }
