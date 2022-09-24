1c969f51942e5b630b844805ce257bfedb01da04
Raymond Jiang <raymondjiang2014@gmail.com>
2022-09-24 19:16:31 +0000

    Added ID to PID_output, also added Clark's fixes to compiler

--- Apps/Src/PID.c
+++ Apps/Src/PID.c
@@ -3,12 +3,19 @@
 #include "PID.h"
 #include "Tasks.h"
 
-uint32_t Proportion,Integral,Derivative;
+#define Proportion 1000     
+#define Integral 1000
+#define Derivative 1000
 
-
+static int32_t area = 0;
+int32_t err;
+int32_t rate;
 //returns fan speed based on proportion value
 int32_t PID_output(int32_t avg_temp, int32_t desired_temp) {
-    return Proportion*(avg_temp - desired_temp);
+    err = desired_temp - avg_temp;
+    area = area + err;
+    
+    return Proportion*(err);
 }
 
 
--- BSP/STM32F413/Src/BSP_CAN.c
+++ BSP/STM32F413/Src/BSP_CAN.c
@@ -149,7 +149,7 @@ ErrorStatus BSP_CAN_Write(uint32_t id, uint8_t data[8], uint8_t length) {
     
     gTxMessage.StdId = id;
     gTxMessage.DLC = length;
-	for(int i = 0; i < length; i++){
+	for(int i = 0; i < length && i < 8; i++){
         gTxMessage.Data[i] = data[i];
     }
 	
