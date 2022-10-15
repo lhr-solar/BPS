8bd5126af0b8abcba7a9efd6e5121d01e52e24e4
Connor <107018918+connorl309@users.noreply.github.com>
2022-10-15 16:59:02 -0500

    Sim v2 cjson rewrite (#487)
    * working on CAN sim
    
    * removed testing lines from sim.c
    
    * pulled changes from clark
    
    * CAN sim logging functional but strings output in reverse order
    
    * removed bad test
    
    * added some real tests, want to reorder the sim structure a bit, waiting on clark
    
    * rewriting simulator.c file with the cJSON functions for readability
    
    * Rewrite works and supports all fields. Provided an emptytest.json file for making sure dummy values fill properly.
    
    * adjusted dummy values to not trip immediately
    
    * adjusting some files real quick

--- BSP/Simulator_V2/Src/Simulator.c
+++ BSP/Simulator_V2/Src/Simulator.c
@@ -20,6 +20,8 @@
 #include "cJSON.h" // for json parser
 #include <stdlib.h> // for calloc/free
 
+#include "BSP_CAN.h" // CAN testing
+
 // file descriptor of simulator log file
 int simulatorLog;
 
@@ -29,6 +31,22 @@ static simulator_state *states = NULL;
 // timing information to handle the state transitions
 // note that this only has a granularity of 1 second, so it's not super precise
 static time_t startTime;
+
+
+// It knows what the input file is because the Makefile should make a #define for the file path called SIMULATOR_JSON_PATH
+#ifndef SIMULATOR_JSON_PATH
+#define JSON_PATH "Error: make sure you define the simulator JSON path in the Makefile!"
+#else
+// #define QUOTE_MARK "
+#define STR(X) # X
+#define STRINGIFY(X) STR(X)
+#define JSON_PATH STRINGIFY(SIMULATOR_JSON_PATH) 
+#endif
+
+// units in mV
+static uint16_t DUMMY_VOLTAGES[NUM_BATTERY_MODULES] = { 3000 };
+// units in mC, but config.h uses 45,000 as 45.00C, so we'll bump these up to 30k for 30C
+static uint16_t DUMMY_TEMPS[NUM_TEMPERATURE_SENSORS] = { 30000 };
 
 // log something to the simualtor's log file
 void Simulator_log(char *str) {
@@ -91,6 +109,8 @@ static void readInputFile(char *jsonPath) {
 
     // parse the json structure into an array of states
     simulator_state *tail = states;
+    uint8_t stateCount = 0;
+    // For every state in the JSON (highest level {...})
     for (cJSON *state = json->child; state != NULL; state = state->next) {
         if (states == NULL) {
             tail = (simulator_state *) calloc(1, sizeof(simulator_state));
@@ -100,46 +120,90 @@ static void readInputFile(char *jsonPath) {
             tail->next = temp;
             tail = tail->next;
         }
-        // iterate through a single simulator state
-        for (cJSON *field = state->child; field != NULL; field = field->next) {
-            if (strcmp(field->string, "time") == 0) {
-                tail->time = field->valueint;
-            } else if (strcmp(field->string, "adcHigh") == 0) {
-                tail->adcHigh = field->valueint;
-            } else if (strcmp(field->string, "adcLow") == 0) {
-                tail->adcLow = field->valueint;
-            } else if (strcmp(field->string, "voltages") == 0) {
-                // get all of the voltages
-                int i = 0;
-                for (cJSON *voltage = field->child; voltage != NULL && i < NUM_BATTERY_MODULES; i++, voltage = voltage->next) {
-                    tail->voltages[i] = voltage->valueint;
-                }
-                if (i != NUM_BATTERY_MODULES) {
-                    printf("Error: incorrect number of voltages! Expected %d, but got %d\n", NUM_BATTERY_MODULES, i);
-                    exit(-1);
-                }
-            } else if (strcmp(field->string, "temperatures") == 0) {
-                // get all of the temperatures
-                int i = 0;
-                for (cJSON *temperature = field->child; 
-                    temperature != NULL && i < (NUM_BATTERY_MODULES * NUM_TEMP_SENSORS_PER_MOD); 
-                    i++, temperature = temperature->next) {
-
-                    tail->temperatures[i] = temperature->valueint;
-                }
-                if (i != (NUM_BATTERY_MODULES * NUM_TEMP_SENSORS_PER_MOD)) {
-                    printf("Error: incorrect number of temperatures! Expected %d, but got %d\n", (NUM_BATTERY_MODULES * NUM_TEMP_SENSORS_PER_MOD), i);
-                    exit(-1);
-                }
-            } else if (strcmp(field->string, "current") == 0) {
-                tail->current = field->valueint;
-            } else if (strcmp(field->string, "charge") == 0) {
-                tail->charge = field->valueint;
-            }else {
-                printf("Error: unknown field %s in simulator state!\n", field->string);
+        char buffer[50];
+        sprintf(buffer, "\nExecuting state #%d...\n", stateCount);
+        Simulator_log(buffer);
+        // Try and get every field we should have in each state.
+        // If null, it doesn't exist.
+        cJSON* time = cJSON_GetObjectItem(state, "time");
+        cJSON* adcHigh = cJSON_GetObjectItem(state, "adcHigh");
+        cJSON* adcLow = cJSON_GetObjectItem(state, "adcLow");
+        // voltageArray should have 31 elements.
+        cJSON* voltageArray = cJSON_GetObjectItem(state, "voltages");
+        // temperatureArray should have 31*2 elements.
+        cJSON* temperatureArray = cJSON_GetObjectItem(state, "temperatures");
+        cJSON* current = cJSON_GetObjectItem(state, "current");
+        cJSON* charge = cJSON_GetObjectItem(state, "charge");
+        cJSON* canList = cJSON_GetObjectItem(state, "can");
+    
+        // Check which state fields aren't present in the current sim state JSON.
+        // Lots of nasty if/else's here, not much way around this.
+        if (!time) {
+            printf("Current simulator state does not have a specified time value. Using -1...\n");
+            tail->time = -1;
+        } else tail->time = time->valueint;
+        if (!adcHigh) {
+            printf("Current simulator state does not have a specified adcHigh value. Using -1...\n");
+            tail->adcHigh = -1;
+        } else tail->adcHigh = adcHigh->valueint;
+        if (!adcLow) {
+            printf("Current simulator state does not have a specified adcLow value. Using -1...\n");
+            tail->adcLow = -1;
+        } else tail->adcLow = adcLow->valueint;
+        if (!voltageArray) { // see top of file for dummy array. units in milliVolts
+            printf("Current simulator state does not have a specified voltage array. Using default...\n");
+            memcpy(tail->voltages, DUMMY_VOLTAGES, sizeof(uint16_t) * NUM_BATTERY_MODULES);
+        } else { // otherwise, the voltage array exists; check size, then copy elements over.
+            if (cJSON_GetArraySize(voltageArray) != NUM_BATTERY_MODULES) {
+                printf("Voltage array in simulator state %d does not have 31 elements in it! Please fix. Exiting...\n", stateCount);
                 exit(-1);
             }
+            for (int idx = 0; idx < NUM_BATTERY_MODULES; idx++) {
+                tail->voltages[idx] = cJSON_GetArrayItem(voltageArray, idx)->valueint;
+            }
         }
+        if (!temperatureArray) { // see top of file for dummy array. units in milliCelsius
+            printf("Current simulator state does not have a specified temperature array. Using default...\n");
+            memcpy(tail->temperatures, DUMMY_TEMPS, sizeof(uint16_t) * NUM_TEMPERATURE_SENSORS);
+        } else { // otherwise, the temperature array exists; check size, then copy elements over.
+            if (cJSON_GetArraySize(temperatureArray) != NUM_TEMPERATURE_SENSORS) {
+                printf("Temperature array in simulator state %d does not have 62 elements in it! Please fix. Exiting...\n", stateCount);
+                exit(-1);
+            }
+            for (int idx = 0; idx < NUM_TEMPERATURE_SENSORS; idx++) {
+                tail->temperatures[idx] = cJSON_GetArrayItem(temperatureArray, idx)->valueint;
+            }
+        }
+        if (!current) { // current in mA
+            printf("Current simulator state does not have a specified current value. Using 30...\n");
+            tail->current = 30;
+        }
+        if (!charge) {
+            printf("Current simulator state does not have a specified charge value. Using 25,000,000...\n");
+            tail->charge = 25000000;
+        }
+        if (!canList) {
+            char buffer[67];
+            sprintf(buffer, "No CAN messages to simulate in this state. (State Count = %d)\n", stateCount);
+            Simulator_log(buffer);
+        } else { // otherwise, there are some potential CAN messages. 
+            // for every CAN message...
+            for (cJSON* msg = canList->child; msg != NULL; msg = msg->next) {
+                uint32_t id = cJSON_GetObjectItem(msg, "id")->valueint;
+                char* read_write = cJSON_PrintUnformatted(cJSON_GetObjectItem(msg, "read_write"));
+                uint8_t* data = (uint8_t*)cJSON_PrintUnformatted(cJSON_GetObjectItem(msg, "data"));
+                uint8_t len = cJSON_GetObjectItem(msg, "len")->valueint;
+                if (strcmp(read_write, "\"read\"") == 0)
+                    BSP_CAN_Read(&id, data);
+                else if (strcmp(read_write, "\"write\"") == 0)
+                    BSP_CAN_Write(id, data, len);
+                else {
+                    printf("Error: CAN message read/write type invalid! [%s]\n", read_write);
+                    exit(-1);
+                }
+            }
+        }
+        stateCount++; // keep track of which state we are on.
     }
 }
 
@@ -166,17 +230,17 @@ void Simulator_init(char *jsonPath) {
         printf("error writing file %s\n", jsonPath);
         exit(-1);
     }
-
+    
     // register the Ctrl-C handler
     sigset_t s;
     sigemptyset(&s);
     const struct sigaction act = {.sa_handler = CtrlCHandler, .sa_mask = s, .sa_flags = 0};
     sigaction(SIGINT, &act, NULL);
 
+    Simulator_log("Simulator intialized\n");    
+
     // initialize the fake inputs
     readInputFile(jsonPath);
-
-    Simulator_log("Simulator intialized\n");
 
     // log the starting time
     startTime = time(NULL);
