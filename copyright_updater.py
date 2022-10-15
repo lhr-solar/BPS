d436729148df914a1e98f0c3f6fca6ffbdce1d9f
Raymond Jiang <raymondjiang2014@gmail.com>
2022-10-15 21:30:20 +0000

    fixed comments

--- copyright_updater.py
+++ copyright_updater.py
@@ -8,9 +8,9 @@ headers = ['/* Copyright (c) 2018-2022 UT Longhorn Racing Solar */\n', '# Copyri
 
 for root, subdirectories, files in os.walk(directory):
     for file in files:
-        if file.endswith(".c") or file.endswith('.h') or file.endswith('.py'):
+        if file.endswith(".c") or file.endswith('.h') or file.endswith('.py'):  #finds all .c .h .py files
             filepath = os.path.join(root, file)
-            f = open(filepath,"r",encoding = "ISO-8859-1")
+            f = open(filepath,"r",encoding = "ISO-8859-1")  #meant to fix bug with not recognizing byte 0x92
             text = f.readlines()
             if "Copyright (c)" in text[0] and "Longhorn Racing Solar" in text[0]:
                 if (text[0][0] == '/' and text[0][1] == '*') or (text[0][0] == '/' and text[0][1] == '/'):
