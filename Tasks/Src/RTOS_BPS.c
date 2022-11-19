cc4d39d738ead23aa3b68c6fa02d3fd30522f078
Raymond Jiang <raymondjiang2014@gmail.com>
2022-11-19 16:47:07 +0000

    Revert "removed rtos"
    This reverts commit 8d8c8296ddaa46de69b5c444ba8b4aabb75dfe5e.

--- Tasks/Src/RTOS_BPS.c
+++ Tasks/Src/RTOS_BPS.c
@@ -13,8 +13,17 @@ void RTOS_BPS_SemPost(void) {
 void RTOS_BPS_MutexPend(void){
 
 }
-void RTOS_BPS_MutexPost(void) {
 
+/**
+ * @brief   Posts the specified Mutex. (For future reference, Post is the same as Give)
+ * @param   *mutex - pointer to the specified RTOS Mutex object
+ * @param   options - a parameter which determines what kind of Post MutexPost performs
+ * @return  none
+ */
+void RTOS_BPS_MutexPost(BPS_OS_MUTEX* mutex, BPS_OS_OPT options) {
+    BPS_OS_ERR err;
+    OSMutexPost(mutex, options, &err);
+    assertOSError(err);
 }
 void RTOS_BPS_TaskCreate(void){
 
