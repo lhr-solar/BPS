8d8c8296ddaa46de69b5c444ba8b4aabb75dfe5e
Raymond Jiang <raymondjiang2014@gmail.com>
2022-11-19 16:41:34 +0000

    removed rtos

--- Tasks/Inc/RTOS_BPS.h
+++ Tasks/Inc/RTOS_BPS.h
@@ -3,23 +3,9 @@ RTOS_BPS library includes all the wrapper functions for a functioning RTOS - nee
 */
 
 //need to add the proper #include files 
-#include "os.h" // for RTOS stuff
-#include "Tasks.h" // for OS errors
 
 #ifndef RTOS_BPS_H
 #define RTOS_BPS_H
-
-// Custom typedefs and options for mutex/semaphore functionality
-// ONLY change the first column in the TypeDef depending on the RTOS used
-
-// Currently using Micrium
-typedef OS_MUTEX    BPS_OS_MUTEX;
-typedef OS_SEM      BPS_OS_SEM;
-typedef OS_OPT      BPS_OS_OPT;
-typedef OS_ERR      BPS_OS_ERR;
-typedef OS_SEM      BPS_OS_SEM;
-typedef OS_TCB      BPS_OS_TCB;
-
 
 /**
  * @brief
@@ -43,12 +29,11 @@ void RTOS_BPS_SemPost(void);
 void RTOS_BPS_MutexPend(void);
 
 /**
- * @brief   Posts the specified Mutex
- * @param   *mutex - pointer to the specified RTOS Mutex object
- * @param   options - a parameter which determines what kind of Post MutexPost performs
- * @return  none
+ * @brief
+ * @param
+ * @return
  */
-void RTOS_BPS_MutexPost(BPS_OS_MUTEX* mutex, BPS_OS_OPT options);
+void RTOS_BPS_MutexPost(void);
 
 /**
  * @brief   
