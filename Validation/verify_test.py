88a8320fd10b49e2c61eeff3f555c45a241d41de
Champers5000 <champers5000@gmail.com>
2023-01-31 18:31:23 -0600

    change ulimit

--- Validation/verify_test.py
+++ Validation/verify_test.py
@@ -93,6 +93,7 @@ def main():
     log_filepath = LOG_FILEPATH + log_filepath + ".log"
     
     #run the simulator 
+    os.system("ulimit -r unlimited")
     os.system("./bps-simulator.out "+ test_name)
 
     #run the verification
