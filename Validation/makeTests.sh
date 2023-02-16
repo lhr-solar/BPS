199013ca24fda01a80ef569e6e06c1d658f9163f
Manthan Upadhyaya <manthanand@gmail.com>
2023-02-16 11:36:18 -0600

    Made good script to make all test files

---
+++ Validation/makeTests.sh
@@ -0,0 +1,18 @@
+errors=0
+for x in Tests/Test_*; do
+    make clean
+    x="${x%.*}"; x="${x#*_}"
+    sim=''
+    if [[ "$x" == *"Simulator"* ]]; then
+        sim='simulator'
+    fi
+    if ! make "$sim" TEST="$x"; then # Different command for STM
+        echo make "$sim" TEST="$x"
+        errors=$((errors+1))
+    fi
+done
+
+if ((errors > 0)); then
+    echo "Errors occurred during testing"
+    exit 1
+fi
