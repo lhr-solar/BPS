b540e25369c3531e5e8a2f8f636bc69361092d84
Manthan Upadhyaya <55002301+manthanand@users.noreply.github.com>
2023-02-27 22:44:40 -0600

    Make tests workflow (#543)
    * Made things easier to debug
    
    * Fixing all test make issues. Need to make script to run simulator tests
    
    * Made good script to make all test files
    
    * Add sudo so workflow works
    
    * debugging cwd
    
    * shell env
    
    * explicitly run sh

--- Tests/Test_SimulatorCAN.c
+++ Tests/Test_SimulatorCAN.c
@@ -14,7 +14,7 @@
  */
 
 int main(int argc, char **argv) {
-    Simulator_Init(argv[1]);
+    Simulator_Init("cantest");
     BSP_CAN_Init(NULL, NULL, false);
     BSP_CAN_Write(0, (uint8_t*)"ABCDEFGH", 8);
     BSP_CAN_Write(1, (uint8_t*)"LHRSOLAR", 8);
