cc8ae031e6d91d0cb384d65d143cf225f500dedb
Champers <champers5000@gmail.com>
2023-03-25 16:35:20 -0500

    python is python

--- Validation/Simulator_Tests.sh
+++ Validation/Simulator_Tests.sh
@@ -1,13 +1,13 @@
-sudo bash Validation/verify_test.py BSP/Simulator/Data/ManualTests/ critical_overcurrent.json
-sudo bash Validation/verify_test.py BSP/Simulator/Data/ManualTests/ critical_overtemp_discharging.json
-sudo bash Validation/verify_test.py BSP/Simulator/Data/ManualTests/ critical_overvolt.json
-sudo bash Validation/verify_test.py BSP/Simulator/Data/ManualTests/ critical_undervolt.json
-sudo bash Validation/verify_test.py BSP/Simulator/Data/ManualTests/ overcurrent.json
-sudo bash Validation/verify_test.py BSP/Simulator/Data/ManualTests/ overtemp_charging.json
-sudo bash Validation/verify_test.py BSP/Simulator/Data/ManualTests/ overtemp_discharging.json
-sudo bash Validation/verify_test.py BSP/Simulator/Data/ManualTests/ overvolt.json
-sudo bash Validation/verify_test.py BSP/Simulator/Data/ManualTests/ undervolt.json
-sudo bash Validation/verify_test.py BSP/Simulator/Data/ManualTests/ killswitch.json
+sudo python3 Validation/verify_test.py BSP/Simulator/Data/ManualTests/ critical_overcurrent.json
+sudo python3 Validation/verify_test.py BSP/Simulator/Data/ManualTests/ critical_overtemp_discharging.json
+sudo python3 Validation/verify_test.py BSP/Simulator/Data/ManualTests/ critical_overvolt.json
+sudo python3 Validation/verify_test.py BSP/Simulator/Data/ManualTests/ critical_undervolt.json
+sudo python3 Validation/verify_test.py BSP/Simulator/Data/ManualTests/ overcurrent.json
+sudo python3 Validation/verify_test.py BSP/Simulator/Data/ManualTests/ overtemp_charging.json
+sudo python3 Validation/verify_test.py BSP/Simulator/Data/ManualTests/ overtemp_discharging.json
+sudo python3 Validation/verify_test.py BSP/Simulator/Data/ManualTests/ overvolt.json
+sudo python3 Validation/verify_test.py BSP/Simulator/Data/ManualTests/ undervolt.json
+sudo python3 Validation/verify_test.py BSP/Simulator/Data/ManualTests/ killswitch.json
 echo -e "End of Manual Tests \n\n"
 
 echo "Starting automated tests"
