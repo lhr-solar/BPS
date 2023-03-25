4d85ec134210b1a73a92a0902789501dfd881129
connorl309 <cleu309fw@gmail.com>
2023-03-25 12:35:05 -0500

    wip

--- BSP/Simulator/Src/Simulator.c
+++ BSP/Simulator/Src/Simulator.c
@@ -64,13 +64,7 @@ void Simulator_Log(LoggingType_t lvl, char *str) {
     char prefix[128];
     strcpy(prefix, LoggingLUT[lvl]); //This is because strcat cannot concat const
     char* msg = strcat(prefix, str);
-    if(OS_Started){
-        RTOS_BPS_MutexPend(&SimMutex, OS_OPT_PEND_BLOCKING);
-    }
     write(simulatorLog, msg, strlen(msg));
-    if(OS_Started){
-        RTOS_BPS_MutexPost(&SimMutex, OS_OPT_POST_ALL);
-    }
     printf("%s", msg);
 }
 
@@ -275,6 +269,10 @@ static void Simulator_Transition(void) {
     // advance to the current state
     time_t currentTime = time(NULL);
     //wait until time for state is completed before moving to next state
+    if (OS_Started) {
+        RTOS_BPS_MutexPend(&SimMutex, OS_OPT_PEND_BLOCKING);
+    }
+
     while (startTime + states->time < currentTime) {
         simulator_state *prev = states;
         states = states->next;
@@ -285,6 +283,9 @@ static void Simulator_Transition(void) {
             Simulator_Log(LOG, "\nFinished last state!\n");
             Simulator_Shutdown(0);
         }
+    }
+    if (OS_Started) {
+        RTOS_BPS_MutexPost(&SimMutex, OS_OPT_POST_ALL);
     }
 }
 
