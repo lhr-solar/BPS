81e027a500ea85c6c30234bd787cc69a46461227
Champers <champers5000@gmail.com>
2023-03-25 16:18:26 -0500

    maxparallel change in simple-tests

--- Validation/Simulator_Tests.sh
+++ Validation/Simulator_Tests.sh
@@ -1,12 +1,15 @@
-sudo bash Validation/test.sh critical_overcurrent.json
-sudo bash Validation/test.sh critical_overtemp_discharging.json
-sudo bash Validation/test.sh critical_overvolt.json
-sudo bash Validation/test.sh critical_undervolt.json
-sudo bash Validation/test.sh overcurrent.json
-sudo bash Validation/test.sh overtemp_charging.json
-sudo bash Validation/test.sh overtemp_discharging.json
-sudo bash Validation/test.sh overvolt.json
-sudo bash Validation/test.sh undervolt.json
-sudo bash Validation/test.sh killswitch.json
+sudo bash Validation/verify_test.py BSP/Simulator/Data/ManualTests/ critical_overcurrent.json
+sudo bash Validation/verify_test.py BSP/Simulator/Data/ManualTests/ critical_overtemp_discharging.json
+sudo bash Validation/verify_test.py BSP/Simulator/Data/ManualTests/ critical_overvolt.json
+sudo bash Validation/verify_test.py BSP/Simulator/Data/ManualTests/ critical_undervolt.json
+sudo bash Validation/verify_test.py BSP/Simulator/Data/ManualTests/ overcurrent.json
+sudo bash Validation/verify_test.py BSP/Simulator/Data/ManualTests/ overtemp_charging.json
+sudo bash Validation/verify_test.py BSP/Simulator/Data/ManualTests/ overtemp_discharging.json
+sudo bash Validation/verify_test.py BSP/Simulator/Data/ManualTests/ overvolt.json
+sudo bash Validation/verify_test.py BSP/Simulator/Data/ManualTests/ undervolt.json
+sudo bash Validation/verify_test.py BSP/Simulator/Data/ManualTests/ killswitch.json
 echo -e "End of Manual Tests \n\n"
-sudo bash Validation/automatedTest.sh
+
+echo "Starting automated tests"
+rm -rf ../BSP/Simulator/Simulator-Out/
+./Automatic_Tester.out 9
--- Validation/test.sh
+++
@@ -1,2 +0,0 @@
-ulimit -r unlimited
-python3 Validation/verify_test.py BSP/Simulator/Data/ManualTests/$1
