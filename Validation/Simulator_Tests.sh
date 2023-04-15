54657e4ffe24f635e6cedeb8cf2b4230b79f4c01
Champers <champers5000@gmail.com>
2023-04-15 13:06:46 -0500

    this shoudl fail too, but more advanced

--- Validation/Simulator_Tests.sh
+++ Validation/Simulator_Tests.sh
@@ -1,19 +1,23 @@
 ulimit -r unlimited
 
-set -e # to exit bash script upon any commmand failing
+#set -e # to exit bash script upon any commmand failing
 
-python3 Validation/verify_test.py BSP/Simulator/Data/ManualTests/critical_overcurrent.json
-python3 Validation/verify_test.py BSP/Simulator/Data/ManualTests/critical_overtemp_discharging.json
-python3 Validation/verify_test.py BSP/Simulator/Data/ManualTests/critical_overvolt.json
-python3 Validation/verify_test.py BSP/Simulator/Data/ManualTests/critical_undervolt.json
-python3 Validation/verify_test.py BSP/Simulator/Data/ManualTests/overcurrent.json
-python3 Validation/verify_test.py BSP/Simulator/Data/ManualTests/overtemp_charging.json
-python3 Validation/verify_test.py BSP/Simulator/Data/ManualTests/overtemp_discharging.json
-python3 Validation/verify_test.py BSP/Simulator/Data/ManualTests/overvolt.json
-python3 Validation/verify_test.py BSP/Simulator/Data/ManualTests/undervolt.json
-python3 Validation/verify_test.py BSP/Simulator/Data/ManualTests/killswitch.json
+yell() { echo "$0: $*" >&2; }
+die() { yell "$*"; exit 111; }
+try() { "$@" || die "$* has failed"; }
+
+try python3 Validation/verify_test.py BSP/Simulator/Data/ManualTests/critical_overcurrent.json
+try python3 Validation/verify_test.py BSP/Simulator/Data/ManualTests/critical_overtemp_discharging.json
+try python3 Validation/verify_test.py BSP/Simulator/Data/ManualTests/critical_overvolt.json
+try python3 Validation/verify_test.py BSP/Simulator/Data/ManualTests/critical_undervolt.json
+try python3 Validation/verify_test.py BSP/Simulator/Data/ManualTests/overcurrent.json
+try python3 Validation/verify_test.py BSP/Simulator/Data/ManualTests/overtemp_charging.json
+try python3 Validation/verify_test.py BSP/Simulator/Data/ManualTests/overtemp_discharging.json
+try python3 Validation/verify_test.py BSP/Simulator/Data/ManualTests/overvolt.json
+try python3 Validation/verify_test.py BSP/Simulator/Data/ManualTests/undervolt.json
+try python3 Validation/verify_test.py BSP/Simulator/Data/ManualTests/killswitch.json
 echo -e "End of Manual Tests \n\n"
 
 echo "Starting automated tests"
 rm -rf ../BSP/Simulator/Simulator-Out/
-./Automatic_Tester.out 9
+try ./Automatic_Tester.out 9
