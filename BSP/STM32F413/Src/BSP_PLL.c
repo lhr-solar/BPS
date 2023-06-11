d67ee7cfe2318fae2e77cc9a8c42686e85603851
connorl309 <cleu309fw@gmail.com>
2023-06-11 09:37:16 -0500

    fix indentation

--- BSP/STM32F413/Src/BSP_PLL.c
+++ BSP/STM32F413/Src/BSP_PLL.c
@@ -25,50 +25,50 @@
  */
 ErrorStatus BSP_PLL_Init(void) {
     RCC_HSEConfig(RCC_HSE_ON);
-	
-	ErrorStatus status = RCC_WaitForHSEStartUp();
-	uint8_t pllAttempts = 0;
+    
+    ErrorStatus status = RCC_WaitForHSEStartUp();
+    uint8_t pllAttempts = 0;
 
-	// If PLL fails to engage in time retry PLL_ATTEMPTS_MAX amount of times
-	// If still fail then we will fault
-	while (status == ERROR) {
-		if (pllAttempts >= PLL_ATTEMPTS_MAX) {
-			//return an error for waiting too long
-			return ERROR;
-		}
-		pllAttempts++;
-	}
+    // If PLL fails to engage in time retry PLL_ATTEMPTS_MAX amount of times
+    // If still fail then we will fault
+    while (status == ERROR) {
+        if (pllAttempts >= PLL_ATTEMPTS_MAX) {
+            //return an error for waiting too long
+            return ERROR;
+        }
+        pllAttempts++;
+    }
 
-	// Flash 3 wait state, prefetch buffer and cache ON
-	FLASH_SetLatency(FLASH_Latency_3);
-	FLASH_PrefetchBufferCmd(ENABLE);
-	FLASH_InstructionCacheCmd(ENABLE);
-	FLASH_DataCacheCmd(ENABLE);
-		
-	// HCLK = SYSCLK
-	RCC_HCLKConfig(RCC_SYSCLK_Div1);
-	
-	// PCLK2 = HCLK/2
-	RCC_PCLK2Config(RCC_HCLK_Div2);
-		
-	// PCLK1 = HCLK/4
-	RCC_PCLK1Config(RCC_HCLK_Div4);
-		
-	// Configure the main PLL clock to 80 MHz
-	RCC_PLLConfig(RCC_PLLSource_HSE, PLL_M, PLL_N, PLL_P, PLL_Q, 2);	// 2 is arbitrary number
-		
-	// Enable the main PLL
-	RCC_PLLCmd(ENABLE);
-		
-	// Wait till the main PLL is ready
-	while (RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET);
-		
-	// Select the main PLL as system clock source
-	RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);
-		
-	// Update the system clock variable
-	SystemCoreClockUpdate();
-	return SUCCESS;
+    // Flash 3 wait state, prefetch buffer and cache ON
+    FLASH_SetLatency(FLASH_Latency_3);
+    FLASH_PrefetchBufferCmd(ENABLE);
+    FLASH_InstructionCacheCmd(ENABLE);
+    FLASH_DataCacheCmd(ENABLE);
+        
+    // HCLK = SYSCLK
+    RCC_HCLKConfig(RCC_SYSCLK_Div1);
+    
+    // PCLK2 = HCLK/2
+    RCC_PCLK2Config(RCC_HCLK_Div2);
+        
+    // PCLK1 = HCLK/4
+    RCC_PCLK1Config(RCC_HCLK_Div4);
+        
+    // Configure the main PLL clock to 80 MHz
+    RCC_PLLConfig(RCC_PLLSource_HSE, PLL_M, PLL_N, PLL_P, PLL_Q, 2);	// 2 is arbitrary number
+        
+    // Enable the main PLL
+    RCC_PLLCmd(ENABLE);
+        
+    // Wait till the main PLL is ready
+    while (RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET);
+        
+    // Select the main PLL as system clock source
+    RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);
+        
+    // Update the system clock variable
+    SystemCoreClockUpdate();
+    return SUCCESS;
 }
 
 /**
@@ -88,10 +88,10 @@ uint32_t BSP_PLL_GetSystemClock(void) {
 void BSP_PLL_DelayU(uint32_t micro)
 {
     uint32_t delay = BSP_PLL_GetSystemClock() / 1000000;
-	for(volatile uint32_t i = 0; i < micro; i++)
-	{
-		for(volatile uint32_t j = 0; j < delay; j++);
-	}
+    for(volatile uint32_t i = 0; i < micro; i++)
+    {
+        for(volatile uint32_t j = 0; j < delay; j++);
+    }
 }
 
 /**
@@ -102,8 +102,8 @@ void BSP_PLL_DelayU(uint32_t micro)
 void BSP_PLL_DelayM(uint32_t milli)
 {
   uint32_t delay = BSP_PLL_GetSystemClock() / 1000;
-	for(volatile uint32_t i = 0; i < milli; i++)
-	{
-		for(volatile uint32_t j = 0; j < delay; j++);
-	}
+    for(volatile uint32_t i = 0; i < milli; i++)
+    {
+        for(volatile uint32_t j = 0; j < delay; j++);
+    }
 }
